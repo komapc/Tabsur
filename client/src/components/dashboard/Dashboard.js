e01bfe536c8af5e12ca9676927eb12bfb4c66fcb
Mark <komapc@gmail.com>
2020-02-12 13:56:48 +0200

    GUI

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -17,19 +17,14 @@ class Dashboard extends Component {
         <div className="row">
           <div className="landing-copy col s12 center-align">
             <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
+              Hey there, {user.name.split(" ")[0]}
               <p className="flow-text grey-text text-darken-1">
-               List of meals will be here{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+              Welcome to <b>Coolanu</b>, the food sharing app.
+
               </p>
             </h4>
             <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
+              
               onClick={this.onLogoutClick}
               className="btn btn-large waves-effect waves-light hoverable blue accent-3"
             >
