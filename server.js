7f9d8567c9dc09d052b010e93a4f0863b3dd708b
Mark <komapc@gmail.com>
2020-02-19 11:33:42 +0200

    deploy?

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const meals= require("./routes/api/meals");
@@ -39,6 +40,16 @@ app.use("/api/users", users);
 app.use("/api/meals", meals);
 app.use("/api/attends", attends);
 
+
+//serve static assets in production
+if (process.env.MODE_ENV === "production")
+{
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	})
+     
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
