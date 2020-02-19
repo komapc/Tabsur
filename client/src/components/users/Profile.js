aeeed5e32b4ce6a260c5b873c2f62f421ab27dc0
Mark <komapc@gmail.com>
2020-02-20 00:11:10 +0200

    add missing files

---
+++ client/src/components/users/Profile.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
+import classnames from "classnames";
+import axios from 'axios';
+
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user_id:this.props.match.params.id,
+            errors: {}
+        };
+
+        axios.get('/api/users/get/' + this.props.match.params.id)
+            .then(res => {
+            console.log(res.data);
+            this.setState({ user: res.data });
+            
+      });
+    }
+
+
+  render() {
+    const { errors } = this.state;
+    if (!this.state.user)
+    {
+        return <div> Loading </div>  
+	}
+    return (
+        
+      <div className="container">
+        <div className="row">
+          <h1>Profile</h1>
+          <div className="col s8 offset-s2">
+           
+              <div className="input-field col s12">
+               User name: {this.state.user.name}
+              </div>
+              <div className="input-field col s12">
+               User address: {this.state.user.address || "unknown"}
+              </div>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+Profile.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(withRouter(Profile));
