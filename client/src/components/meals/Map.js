a9e2228f6ae02805e85481db8c39bab1d990098b
Mark <komapc@gmail.com>
2020-02-23 18:15:40 +0200

    improvements in map

--- client/src/components/meals/Map.js
+++ client/src/components/meals/Map.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { withGoogleMap, GoogleMap, withScriptjs, InfoWindow, Marker } from "react-google-maps";
 import Autocomplete from 'react-google-autocomplete';
 import Geocode from "react-geocode";
-Geocode.setApiKey("AIzaSyDGe5vjL8wBmilLzoJ0jNIwe9SAuH2xS_0");
+Geocode.setApiKey("AIzaSyBxcuGXRxmHIsiI6tDQDVWIgtGkU-CHZ-4");
 Geocode.enableDebug();
 class Map extends React.Component {
   constructor(props) {
@@ -236,26 +236,9 @@ class Map extends React.Component {
     let map;
     if (this.props.center.lat !== undefined) {
       map = <div>
-        <div>
-          <div className="form-group">
-            <label htmlFor="">City</label>
-            <input type="text" name="city" className="form-control" onChange={this.onChange} readOnly="readOnly" value={this.state.city} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="">Area</label>
-            <input type="text" name="area" className="form-control" onChange={this.onChange} readOnly="readOnly" value={this.state.area} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="">State</label>
-            <input type="text" name="state" className="form-control" onChange={this.onChange} readOnly="readOnly" value={this.state.state} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="">Address</label>
-            <input type="text" name="address" className="form-control" onChange={this.onChange} readOnly="readOnly" value={this.state.address} />
-          </div>
-        </div>
+        
         <AsyncMap
-          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGe5vjL8wBmilLzoJ0jNIwe9SAuH2xS_0&libraries=places"
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBxcuGXRxmHIsiI6tDQDVWIgtGkU-CHZ-4&libraries=places"
           loadingElement={
             <div style={{ height: `100%` }} />
           }
