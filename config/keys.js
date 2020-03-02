f06cf11d2aac3e45cd2eae48242def3af273aec5
mark <komapc@gmail.com>
2020-03-02 14:49:12 +0200

    google key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI: "mongodb+srv://tasur:Dzirciema93@cluster0-pnv4h.gcp.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "Dzirciema93"
+  secretOrKey: "Dzirciema93",
+  google: "AIzaSyBxcuGXRxmHIsiI6tDQDVWIgtGkU-CHZ-4"
 };
