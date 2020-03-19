eb00b951e5aced2f9691ca278f9b72f6507ae50d
mark <komapc@gmail.com>
2020-03-19 11:58:02 +0200

    pgConfig

--- routes/dbConfig.js
+++ routes/dbConfig.js
@@ -1,8 +1,10 @@
-export default  pgConfig = {
+module.exports = {
+   pgConfig : {
     host: '127.0.0.1',
     port: 5432,
     database: 'coolanu',
     user: 'coolanu',
     password: 'coolanu',
     ssl: true,
+}
 };
