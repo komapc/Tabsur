1bb025730e3a3bfd95ee455ea08e584e340c83df
mark <komapc@gmail.com>
2020-03-19 11:55:23 +0200

    warning

--- client/src/components/notifications/Notifications.js
+++ client/src/components/notifications/Notifications.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import axios from 'axios';
 
 class Notifications extends Component {
-
 
   constructor(props) {
     super(props);
