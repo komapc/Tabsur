b441d7a37d4c96caa2ceb339d6df1cdaf14804ab
mark <komapc@gmail.com>
2020-03-22 00:44:35 +0200

    meals: postgres

--- routes/api/meals.js
+++ routes/api/meals.js
@@ -1,3 +1,5 @@
+const pgConfig=require("./../dbConfig.js");
+const {Client}=require("pg");
 const express = require("express");
 const router = express.Router();
 const keys = require("../../config/keys");
@@ -11,37 +13,59 @@ const Meal = require("../../models/Meal")
 // @route GET api/meals/get
 // @desc get a meal list
 // @access Public
-router.get("/get", (req, res) => 
+router.get("/get", async  (req, response) => 
 {
-  // Find meals
-  myCursor = Meal.find(function(err, meals) 
-  {
-    if (err) 
-    {
-        console.log(err);
-    } else 
-    {
-        res.json(meals);
-    }
-  })
-});
+  const client = new Client(pgConfig.pgConfig);
 
+  const meal = req.body;
+  console.log(meal);
+
+  await client.connect();
+
+  await client.query('SELECT * from   meals '
+     // [meal.name, meal.type, `(${meal.location.lng}, ${meal.location.lat})`, meal.address, meal.guestCount, /*meal.host*/ 42]);
+     , (err, resp) => {
+      if (err) {
+        console.log(err.stack)
+      } else {
+        console.log(resp.rows[0]);
+        response.json(resp.rows);
+      }
+    })
+})
+ 
 // @route GET api/meals/get_my
 // @desc get a list of meals created by me
 // @access Public
-router.get("/get_my/:id", (req, res) => 
+router.get("/get_my/:id", async (req, res) => 
 {
-    // Find meals
-    myCursor = Meal.find({host:req.params.id}, function(err, meals) 
-    {
-        if (err) 
-        {
-            console.log(err);
-        } else 
-        {
-            res.json(meals);
-        }
+  const client = new Client(pgConfig.pgConfig);
+
+  const meal = req.body;
+  console.log(meal);
+
+  await client.connect();
+
+  await client.query(`SELECT * from   meals where host_id=${req.params.id}`, (err, resp) => {
+      if (err) {
+        console.log(err.stack)
+      } else {
+        console.log(resp.rows[0]);
+        response.json(resp.rows);
+      }
     })
+
+    // // Find meals
+    // myCursor = Meal.find({host:req.params.id}, function(err, meals) 
+    // {
+    //     if (err) 
+    //     {
+    //         console.log(err);
+    //     } else 
+    //     {
+    //         res.json(meals);
+    //     }
+    // })
 });
 
 
@@ -72,38 +96,35 @@ router.get('/get/:id', function(req, res, next) {
 
 });
 
-// TODO: Move to config file
-const pgConfig = {
-    host: '127.0.0.1',
-    port: 5432,
-    database: 'coolanu',
-    user: 'coolanu',
-    password: 'coolanu',
-    ssl: true,
-};
-const {Client} = require('pg');
-const client = new Client(pgConfig);
-
 // @route POST api/meals/addMeal
 router.post("/addMeal", async (req, response) => {
     // Form validation
-
+    const client = new Client(pgConfig.pgConfig);
     const { errors, isValid } = validateMealInput(req.body);
 
     // Check validation
     if (!isValid) {
         return response.status(400).json(errors);
     }
-
+    try{
+      
+    console.log("addMeal - start,  " +pgConfig.pgConfig.user );
     const meal = req.body;
     await client.connect();
-
+    
+    console.log("connected");
     await client.query('INSERT INTO meals (name, type, location, address, guest_count, host_id)' +
         'VALUES($1, $2, $3, $4, $5, $6)',
-        [meal.name, meal.type, `(${meal.location.lng}, ${meal.location.lat})`, meal.address, meal.guestCount, /*meal.host*/ 42]);
+        [meal.name, meal.type, `(${meal.location.lng}, ${meal.location.lat})`, meal.address, meal.guestCount, meal.host_id]);
     // TODO: fix user id
-
-    client.end();
+    
+    }
+    catch(e)
+    {
+      console.log("exception catched: " + e);
+    }
+    console.log("query done");
+    await client.end();
     return response.status(201).json(req.body);
 });
 
@@ -112,9 +133,19 @@ router.post("/addMeal", async (req, response) => {
 // @desc delete a meal
 // @access Public (?)
 
-router.get("/delete/:id", (req, res) => 
+router.get("/delete/:id",async(req, res) => 
 { 
 //todo
+
+const client = new Client(pgConfig);
+  await client.connect();
+
+  await client.query('delete from meals where id=$1',
+      [req.id]);
+  // TODO: fix user id
+
+  client.end();
+
   // Find meal by name
   myCursor = Meal.delete(function(err, meals) 
   {
