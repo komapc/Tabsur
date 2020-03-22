2129f0d06018e4b2840327900faa6f1bd4c249d9
mark <komapc@gmail.com>
2020-03-23 00:50:06 +0200

    add bottom

---
+++ client/src/components/layout/Bottom.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="z-depth-0">
+          <div >
+           
+           bottom
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
