d7025a46db2bb7cf9796a4594008ea8f5f2f32b9
mark <komapc@gmail.com>
2020-03-23 14:54:59 +0200

    remove long/lat from create screen

--- client/src/components/meals/CreateMeal.js
+++ client/src/components/meals/CreateMeal.js
@@ -81,15 +81,8 @@ class CreateMeal extends Component {
                   id="address"
                   type="text"
                 />
-                <label htmlFor="location">Address</label>
+                <label htmlFor="address">Address</label>
                 <span className="red-text">{errors.name}</span>
-              </div>
-              {/* Location */}
-              <div className="col s12">
-                <div>
-                  Lat: {this.state.location.lat}, lng: {this.state.location.lng}
-                </div>
-                <label htmlFor="location">Location</label>
               </div>
               {/* Number of guests */}
               <div className="input-field col s12">
