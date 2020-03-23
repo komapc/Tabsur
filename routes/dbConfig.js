c25de4b7876de5a669451dee6082be86a044913f
mark <komapc@gmail.com>
2020-03-23 17:06:11 +0200

    production - db user/pw

--- routes/dbConfig.js
+++ routes/dbConfig.js
@@ -10,9 +10,9 @@ module.exports = {
     pgConfigProduction : {
     host: 'coolanu.c83ccb9pqvdz.us-east-1.rds.amazonaws.com',
     port: 5432,
-    database: 'coolanu',
-    user: 'coolanu',
-    password: 'coolanu',
+    database: 'postgres',
+    user: 'production_db',
+    password: 'jw8s0F4',
     ssl: false,
 }
 };
