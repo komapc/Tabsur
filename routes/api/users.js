ffe716b138bfaa6e19596cc239dd1087c48f22fb
mark <komapc@gmail.com>
2020-03-24 22:59:35 +0200

    move file

--- routes/api/users.js
+++ routes/api/users.js
@@ -32,6 +32,7 @@ router.post("/register", async (req, response) => {
 
   // Check validation
   if (!isValid) {
+    console.log("invalid input: " + JSON.stringify(errors));
     return response.status(400).json(errors);
   }
   try {
@@ -45,7 +46,7 @@ router.post("/register", async (req, response) => {
       bcrypt.hash(input.password, salt, async (err, hash) => {
         if (err) throw err;
         //input.password = hash;
-        await client.query('INSERT INTO users (name, email, password, location, address)' +
+        client.query('INSERT INTO users (name, email, password, location, address)' +
           'VALUES ($1, $2, $3, $4, $5)',
           [newUser.name, newUser.email, hash, `33,33`, 'address'])
           .then(user => { return response.status(201).json(newUser); })
@@ -67,15 +68,29 @@ router.post("/register", async (req, response) => {
 router.post("/login", async (req, response) => {
   // Form validation
   const newReq = req.body;
+  const { errors, isValid } = validateLoginInput(req.body);
+  if (!isValid) {
+    console.log("invalid input: " + JSON.stringify(errors));
+    return response.status(400).json(errors);
+  }
   console.log('Login: ' + newReq.email);
   const client = new Client(currentConfig);
   await client.connect();
-  await client.query('select * from  users where email = $1  limit 1',
-    [newReq.email])
-    .then(user => {
-      const row = user.rows[0];
-      console.log("result: " + JSON.stringify(row));
+  await client.query('SELECT password FROM users WHERE email = $1 OR id = $2 LIMIT 1',
+    [newReq.email, newReq.id])
+    .then(res => {
+
+      //no record found
+      if (res.rows === undefined || res.rows.length == 0) {
+        console.log(`error: user doesn't exist`, [newReq.email]); 
+        errors.email = "email not found";//emailnotfound
+        return response.status(500).json(errors);
+      }
+      
       // Check password
+      const row=res.rows[0];
+      console.log('res : ' + JSON.stringify(res));
+      console.log('row : ' + JSON.stringify(row));
       bcrypt.compare(newReq.password, row.password).then(isMatch => {
         if (isMatch) {
           // User matched
@@ -105,7 +120,8 @@ router.post("/login", async (req, response) => {
             .json({ passwordincorrect: "Password incorrect" });
         }
       })
-        .catch(err => { console.log(err); return response.status(500).json(newReq); });
+        .catch(err => { console.log(err); return response.status(500).json(newReq); })
+        .then(() => client.end())
 
     });
 });
