513469c65e7761d185263699318f4f12ccdd2fb8
mark <komapc@gmail.com>
2020-03-24 16:41:45 +0200

    cleanup; remove //mongoURI:

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { setCurrentUser, logoutUser } from "../actions/authActions";
+import { logoutUser } from "../actions/authActions";
 import store from "../store";
 class Menu extends Component {
 
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://tasur:Dzirciema93@cluster0-pnv4h.gcp.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "Dzirciema93",
+    secretOrKey: "Dzirciema93",
   google: "AIzaSyBxcuGXRxmHIsiI6tDQDVWIgtGkU-CHZ-4"
 };
