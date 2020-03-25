f28d03b45516cc66673b662943705d4c7dd17035
mark <komapc@gmail.com>
2020-03-25 20:23:23 +0200

    restore import {   GET_ERRORS,   GET_CURRENT_USER,   SET_CURRENT_USER,   USER_LOADING } from "./types";

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -2,7 +2,12 @@ import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
 
-import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
+import {
+  GET_ERRORS,
+  GET_CURRENT_USER,
+  SET_CURRENT_USER,
+  USER_LOADING
+} from "./types";
 import config from "../config";
 
 // Register User
