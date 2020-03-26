c27993daa0b37a9eb23d316b44c5700bbe88ec85
mark <komapc@gmail.com>
2020-03-26 23:18:34 +0200

    remove logout

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -3,8 +3,10 @@ import { GET_ERRORS } from "../actions/types";
 const initialState = {};
 
 export default function(state = initialState, action) {
+  console.log("error reducer: " + JSON.stringify(action)); 
   switch (action.type) {
     case GET_ERRORS:
+      console.log("error reducer: GET_ERRORS, " + action.payload);
       return action.payload;
     default:
       return state;
