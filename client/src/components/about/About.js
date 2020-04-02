98e80f22e3d670ecb883596e70ada0bfacbf65ff
mark <komapc@gmail.com>
2020-04-02 14:56:49 +0300

    improve about window, get version

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,23 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
-class MyMeals extends Component {
+//import { system } from "../../actions/authActions";
+import axios from 'axios';
+import config from "../../config";
+class About extends Component {
 
 
   constructor(props) {
     super(props);
     this.state = {
-      meals: []
+      user: []
     };
+    // system().then(systemData => {
+    // console.log(systemData);})
+    axios.get(`${config.SERVER_HOST}/api/users/system`)
+    .then(res => {
+      console.log(res.data);
+
+    });
   }
 
   render() {
+    const { user } = this.props.auth;
+    console.log("About log:" + user.name);
     return (
       <div className="container valign-wrapper">
         <div className="row">
           <div className="landing-copy ">
-            <h4>
-              About us.
+            <h4>  Hey, 
+              <small>{user.name}</small>
             </h4>
             <div>
               Welcome to Coolanu TableSurfing community!
@@ -34,5 +45,11 @@ class MyMeals extends Component {
 }
 
 
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
 export default connect(
-)(MyMeals);
+  mapStateToProps,
+)(About);
