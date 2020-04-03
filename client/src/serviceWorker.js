47497c9a7f7287f32d67822a9091a6303ab2d308
mark <komapc@gmail.com>
2020-04-04 01:24:43 +0300

    fix validation; logout; protected routes policy change; warning fix

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -22,7 +22,7 @@ const isLocalhost = Boolean(
 
 export function register(config) {
   console.log("serviceWorker.js, env = " + process.env.NODE_ENV);
-  if (process.env.NODE_ENV != 'debug' && 'serviceWorker' in navigator) {
+  if (process.env.NODE_ENV !== 'debug' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
