3c4d9913a463e9b38d3abd245d99db8230a2a8bf
mark <komapc@gmail.com>
2020-04-05 23:35:28 +0300

    use icons

--- client/src/components/layout/Bottom.js
+++ client/src/components/layout/Bottom.js
@@ -1,44 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import map from "../../resources/map.png"
+import list from "../../resources/list.png"
+import plus from "../../resources/plus.png"
+import myMeals from "../../resources/my.png"
 
 class Bottom extends Component {
-  constructor()
-  {
+  constructor() {
     super();
     this.state = {
-      url:window.location.pathname
+      url: window.location.pathname
     };
   }
   render() {
-   
+
     return (
       <div className="footer ">
         <nav className="z-depth-0 footer">
-          <div > 
-            <Link
-             to="/MealsMap"
-            >
-            <span className={(this.state.url==="/MealsMap")?"footerItem":"footerItemActive"} >Map</span>
+          <Link to="/MealsMap">
+            <img className="footer-icons" src={map} alt={"meals map"} />
+            {/* <img src="" className={(this.state.url==="/MealsMap")?"footerItem":"footerItemActive"} /> */}
+          </Link>
+          <Link to="/Meals" >
+           <img className="footer-icons" src={list} alt={"meals list"} /> </Link>
+           <Link to="/Create" >
+           <img className="footer-icons" src={plus} alt={"add meal"} />
+          </Link>
+          <Link to="/MyMeals">
+            <img className="footer-icons" src={myMeals} alt={"my meals"} />
             </Link>
-            <span> | </span>
-            <Link 
-              to="/Meals"
-            >
-              <span className={(window.location.pathname==="/Meals")?"footerItem":"footerItemActive"} >List</span>
-            </Link>
-            <span> | </span>
-            <Link
-              to="/Create"
-            >
-               <span className={(window.location.pathname==="/Create")?"footerItem":"footerItemActive"} >➕</span>
-            </Link>
-            <span> | </span>
-            <Link
-              to="/MyMeals"
-            >
-              My meals
-            </Link>
-          </div>
         </nav>
       </div>
     );
