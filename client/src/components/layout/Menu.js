80b8ef15adacff36228f9a5834ccbbc123848b06
mark <komapc@gmail.com>
2020-04-09 15:55:11 +0300

    menu inside the screen, not a new frame

--- client/src/components/layout/Menu.js
+++ client/src/components/layout/Menu.js
@@ -3,19 +3,25 @@ import { Link } from "react-router-dom";
 import { logoutUser } from "../../actions/authActions";
 import store from "../../store";
 class Menu extends Component {
-handleLogout(event)
-{
-  store.dispatch(logoutUser());
-}
-render() {
-   
-  return (
-    <div className="main menu">
-      <Link to="/Login" onClick={this.handleLogout}>Logout</Link> <br/> 
-      <Link to="/About">About</Link><br/>
-      <Link to="/MyProfile"> <span>🧍</span>My profile</Link><br/>
-    </div>  
-  );
-}
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu:false
+    };
+  }
+  handleLogout(event) {
+    store.dispatch(logoutUser());
+  }
+  render() {
+
+    return (
+      <div className={this.state.showMenu ? "menu" : "menu-hidden"}>
+        <Link to="/Login" onClick={this.handleLogout}>Logout</Link> <br />
+        <Link to="/About">About</Link><br />
+        <Link to="/MyProfile"> <span>🧍</span>My profile</Link><br />
+      </div>
+    );
+  }
 }
 export default Menu;
