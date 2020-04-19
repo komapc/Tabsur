c25ecb83f3dbfcaaa8a3d0c7b8692062e2b1d24f
mark <komapc@gmail.com>
2020-04-19 20:40:29 +0300

    use svg icons + fix distance

--- client/src/components/layout/Bottom.js
+++ client/src/components/layout/Bottom.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import map from "../../resources/map.png"
-import list from "../../resources/list.png"
-import plus from "../../resources/plus.png"
-import myMeals from "../../resources/my.png"
 
+import map from "../../resources/bottom_menu/map_bar.svg"
+import list from "../../resources/bottom_menu/list_bar.svg"
+import plus from "../../resources/bottom_menu/add_meal_bar.svg"
+import myMeals from "../../resources/bottom_menu/my_meals_bar.svg"
+
+// import mapActive from "../../resources/bottom_menu/active_map_bar.svg"
+// import listActive from"../../resources/bottom_menu/active_list_bar.svg"
+// import plusActive from "../../resources/bottom_menu/active_add_meal_bar.svg"
+// import myMealsActive from "../../resources/bottom_menu/active_my_meals_bar.svg"
 class Bottom extends Component {
   constructor() {
     super();
@@ -19,7 +24,7 @@ class Bottom extends Component {
         <nav className="z-depth-0 footer">
           <Link to="/MealsMap">
             <img className="footer-icons" src={map} alt={"meals map"} />
-            {/* <img src="" className={(this.state.url==="/MealsMap")?"footerItem":"footerItemActive"} /> */}
+             <img src="" className={(this.state.url==="/MealsMap")?"footerItem":"footerItemActive"} /> 
           </Link>
           <Link to="/Meals" >
            <img className="footer-icons" src={list} alt={"meals list"} /> </Link>
