da5146fb0b0a4706309785c8a43a901da6f23e0a
mark <komapc@gmail.com>
2020-04-19 22:43:04 +0300

    bottom icons - fix active

--- client/src/components/layout/Bottom.js
+++ client/src/components/layout/Bottom.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import { NavLink } from 'react-router-dom';
 import map from "../../resources/bottom_menu/map_bar.svg"
 import list from "../../resources/bottom_menu/list_bar.svg"
 import plus from "../../resources/bottom_menu/add_meal_bar.svg"
 import myMeals from "../../resources/bottom_menu/my_meals_bar.svg"
 
-// import mapActive from "../../resources/bottom_menu/active_map_bar.svg"
-// import listActive from"../../resources/bottom_menu/active_list_bar.svg"
-// import plusActive from "../../resources/bottom_menu/active_add_meal_bar.svg"
-// import myMealsActive from "../../resources/bottom_menu/active_my_meals_bar.svg"
 class Bottom extends Component {
   constructor() {
     super();
@@ -17,24 +13,24 @@ class Bottom extends Component {
       url: window.location.pathname
     };
   }
-  render() {
 
+  render() {
     return (
+
       <div className="footer">
-        <nav className="z-depth-0 footer">
-          <Link to="/MealsMap">
-            <img className="footer-icons" src={map} alt={"meals map"} />
-             <img src="" className={(this.state.url==="/MealsMap")?"footerItem":"footerItemActive"} /> 
-          </Link>
-          <Link to="/Meals" >
-           <img className="footer-icons" src={list} alt={"meals list"} /> </Link>
-           <Link to="/Create" >
-           <img className="footer-icons" src={plus} alt={"add meal"} />
-          </Link>
-          <Link to="/MyMeals">
-            <img className="footer-icons" src={myMeals} alt={"my meals"} />
-            </Link>
+        <nav>
+            <NavLink to="/MealsMap" activeClassName="active">
+              <img className="footer-icons" src={map} alt={"meals map"} /></NavLink >
+            <NavLink to="/Meals" activeClassName="active">
+              <img className="footer-icons" src={list} alt={"meals list"} /> </NavLink >
+            <NavLink to="/Create" activeClassName="active">
+              <img className="footer-icons" src={plus} alt={"add meal"} /></NavLink >
+            <NavLink to="/MyMeals" activeClassName="active">
+
+              <img className="footer-icons" src={myMeals} alt={"my meals"} /> </NavLink >
+
         </nav>
+
       </div>
     );
   }
