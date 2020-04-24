91102458a7bb8b8403e745c4bda1130bc288352b
mark <komapc@gmail.com>
2020-04-24 16:33:45 +0300

    Notifications.js

--- client/src/components/layout/Menu.js
+++ client/src/components/layout/Menu.js
@@ -7,7 +7,6 @@ class Menu extends Component {
 
   constructor(props) {
     super(props);
-    console.log("MENU CONSTRUCTOR");
     this.state = {
       visible: this.props.visible,
       onItemClicked:this.props.onItemClicked,
