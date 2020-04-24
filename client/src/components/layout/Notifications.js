30299bb29b926846c5a4b085780485c126aed460
mark <komapc@gmail.com>
2020-04-24 16:05:48 +0300

    add Notifications.js

---
+++ client/src/components/layout/Notifications.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { logoutUser } from "../../actions/authActions";
+import store from "../../store";
+import menu from "../../resources/menu.svg" 
+class Menu extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log("MENU CONSTRUCTOR");
+    this.state = {
+      visible: this.props.visible,
+      onItemClicked:this.props.onItemClicked,
+    };
+  }
+  closeMenu = () =>{
+    this.state.onItemClicked();
+  }
+
+  handleLogout(event) {
+    store.dispatch(logoutUser());
+    this.closeMenu();
+  }
+  render() {
+    const visible = this.props.visible;
+    return (
+      <div className={visible ? "menu" : "menu-hidden"}>
+        <div><img  className="menu-close" src={menu} onClick={this.closeMenu}/></div>
+        <Link className="link" to="/Login" onClick={this.handleLogout}>Logout</Link> <br />
+        <Link className="link" to="/About" onClick={this.closeMenu}>About</Link><br />
+        <Link className="link" to="/MyProfile"  onClick={this.closeMenu}>My profile</Link>
+      </div>
+    );
+  }
+}
+export default Menu;
