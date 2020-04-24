aa3268f8b268744f7ab59de5e4bd8dc46148c4bc
mark <komapc@gmail.com>
2020-04-24 13:41:55 +0300

    close menu

--- client/src/components/layout/Menu.js
+++ client/src/components/layout/Menu.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { logoutUser } from "../../actions/authActions";
 import store from "../../store";
+import menu from "../../resources/menu.svg" 
 class Menu extends Component {
 
   constructor(props) {
@@ -24,6 +25,7 @@ class Menu extends Component {
     const visible = this.props.visible;
     return (
       <div className={visible ? "menu" : "menu-hidden"}>
+        <div><img  className="menu-close" src={menu} onClick={this.closeMenu}/></div>
         <Link className="link" to="/Login" onClick={this.handleLogout}>Logout</Link> <br />
         <Link className="link" to="/About" onClick={this.closeMenu}>About</Link><br />
         <Link className="link" to="/MyProfile"  onClick={this.closeMenu}>My profile</Link>
