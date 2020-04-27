7e03166cd327a6579cf248b3e7084d2de3b0697c
mark <komapc@gmail.com>
2020-04-27 12:59:09 +0300

    default value

--- db/migrations/V8__notifications_add_columns.sql
+++ db/migrations/V8__notifications_add_columns.sql
@@ -4,7 +4,7 @@ ADD COLUMN  message_text  varchar(250),
 ADD COLUMN sender integer;
 
 
-alter table notifications add column status integer; -- 0 - sent; 1 - delivered; 2 - read 
+alter table notifications add column status integer default 0; -- 0 - created; 1 - delivered; 2 - read 
 
 -- 0 - system; 1 - new user; 2 - message; 3 - new meal;
 -- 4 - new hungry; 5 - request approved; 6 - rejected; 7 - reminder
