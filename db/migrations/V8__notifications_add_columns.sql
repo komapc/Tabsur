db8fdef7d64a68a73821fe27e83605173b4fa2cf
mark <komapc@gmail.com>
2020-04-27 12:50:44 +0300

    alter table notifications add column status integer; -- 0 - sent; 1 - delivered; 2 - read
    -- 0 - system; 1 - new user; 2 - message; 3 - new meal;
    -- 4 - new hungry; 5 - request approved; 6 - rejected; 7 - reminder
    alter table notifications add column note_type integer;

--- db/migrations/V8__notifications_add_columns.sql
+++ db/migrations/V8__notifications_add_columns.sql
@@ -1,4 +1,11 @@
 SELECT * from notifications;
 ALTER TABLE notifications
 ADD COLUMN  message_text  varchar(250),
-ADD COLUMN sender integer
+ADD COLUMN sender integer;
+
+
+alter table notifications add column status integer; -- 0 - sent; 1 - delivered; 2 - read 
+
+-- 0 - system; 1 - new user; 2 - message; 3 - new meal;
+-- 4 - new hungry; 5 - request approved; 6 - rejected; 7 - reminder
+alter table notifications add column note_type integer;
