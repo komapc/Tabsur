e0953b124d062726330e5ecad14c45a18fb3db8c
mark <komapc@gmail.com>
2020-04-29 15:44:06 +0300

    GuestList

--- client/src/components/meals/ShowMeal.js
+++ client/src/components/meals/ShowMeal.js
@@ -3,18 +3,34 @@ import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { addMeal, getMeals } from "../../actions/mealActions";
 import MealListItem from "./MealListItem";
+
+class GuestList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = props;
+    this.state.guests=[];
+  }
+  render() {
+    return (
+      <div >
+        Guests list
+      </div>
+    );
+  }
+};
+
 class ShowMeal extends Component {
 
   constructor(props) {
     super(props);
     this.state = props.location.state;
-  //  alert(JSON.stringify(props.location));
   }
 
   render() {
     return (
       <div className="main">
-      <MealListItem meal={this.state.meal} />
+      <MealListItem meal={this.props.meal} />
+      <GuestList mealId={this.props.meal.id}/>
       </div>
     );
   }
