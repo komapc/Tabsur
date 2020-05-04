d09bd501613b02b025790975efcec95f0e3580e1
mark <komapc@gmail.com>
2020-05-04 21:57:39 +0300

    fix + refactor add meal server

--- validation/meal.js
+++ validation/meal.js
@@ -7,7 +7,6 @@ module.exports = function validateMealInput(data) {
   // Convert empty fields to an empty string so we can use validator functions
   data.name = !isEmpty(data.name) ? data.name : "";
 
-
   // Name checks
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
@@ -17,15 +16,14 @@ module.exports = function validateMealInput(data) {
 
   if (Validator.isEmpty(data.address)) {
     errors.address = "Address field is required";
-
-
-    data.guestCount = (guestCount > 0) ? data.guestCount : "";
-    if (guestCount < 0) {
-      errors.guestCount = "Number if invited guests should be a positive number";
-    }
-    return {
-      errors,
-      isValid: isEmpty(errors)
-    };
   }
+
+  data.guestCount = (data.guestCount > 0) ? data.guestCount : "";
+  if (data.guestCount < 0) {
+    errors.guestCount = "Number if invited guests should be a positive number";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
 };
