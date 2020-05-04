9f94b3986016fc9671f1604777d24978368b0d02
mark <komapc@gmail.com>
2020-05-04 12:36:20 +0300

    create meal validation

--- validation/meal.js
+++ validation/meal.js
@@ -13,6 +13,16 @@ module.exports = function validateMealInput(data) {
     errors.name = "Name field is required";
   }
 
+  data.address = !isEmpty(data.address) ? data.address : "";
+
+  if (Validator.isEmpty(data.address)) {
+    errors.address = "Address field is required";
+
+    
+  data.guestCount = (guestCount>0) ? data.guestCount : "";
+  if (guestCount < 0) {
+    errors.guestCount = "Number if invited guests should be a positive number";
+  }
   return {
     errors,
     isValid: isEmpty(errors)
