df4c3664b594a663a859902fa677a03004297def
mark <komapc@gmail.com>
2020-05-13 00:07:04 +0300

    fix warning, give notifications a key

--- client/src/components/layout/Notifications.js
+++ client/src/components/layout/Notifications.js
@@ -2,10 +2,9 @@ import React, { Component } from "react";
 import config from "../../config";
 import { connect } from "react-redux";
 import axios from 'axios';
-import menu from "../../resources/menu.svg" 
+import menu from "../../resources/menu.svg"
 
-class NoteItem extends Component
-{
+class NoteItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,26 +12,24 @@ class NoteItem extends Component
       note: this.props.note
     };
   }
-  
-  markAsRead = (note, status) =>
-  {
+
+  markAsRead = (note, status) => {
     note.status = status;
 
     axios.put(`${config.SERVER_HOST}/api/notifications/` + note.id, note)
-    .then(res => {
-      console.log(res.data);
-    }).catch(err =>{
-      console.log(err);
-    }); 
+      .then(res => {
+        console.log(res.data);
+      }).catch(err => {
+        console.log(err);
+      });
   }
   render() {
-    const visible = this.props.visible;
-    const note=this.props.note;
+    const note = this.props.note;
     return (
-              <div key={note.id} onClick={()=>this.markAsRead(note, 3)}>
-                <div className="notification">{note.message_text} </div>
-              </div>
-     
+      <div onClick={() => this.markAsRead(note, 3)}>
+        <div className="notification">{note.message_text} </div>
+      </div>
+
     );
   }
 };
@@ -42,20 +39,19 @@ class Notifications extends Component {
     super(props);
     this.state = {
       visible: this.props.visible,
-      onItemClicked:this.props.onItemClicked,
+      onItemClicked: this.props.onItemClicked,
       notes: []
     };
   }
 
-  getNotifications = () =>
-  {
+  getNotifications = () => {
     axios.get(`${config.SERVER_HOST}/api/notifications/` + this.props.auth.user.id)
-    .then(res => {
-      console.log(res.data);
-      this.setState({ notes: res.data });
-    }).catch(err =>{
-      console.log(err);
-    }); 
+      .then(res => {
+        console.log(res.data);
+        this.setState({ notes: res.data });
+      }).catch(err => {
+        console.log(err);
+      });
   }
 
 
@@ -64,22 +60,22 @@ class Notifications extends Component {
       this.getNotifications();
     }
   }
-  
+
   componentDidMount() {
     this.getNotifications();
   }
 
-  closeMenu = () =>{
+  closeMenu = () => {
     this.state.onItemClicked();
   }
   render() {
     const visible = this.props.visible;
     return (
       <div className={visible ? "notes" : "notes-hidden"}>
-        <div><img  className="menu-close" src={menu} onClick={this.closeMenu}/></div>
+        <div><img className="menu-close" src={menu} onClick={this.closeMenu} /></div>
         {this.state.notes.map(note =>
-        <NoteItem   onClick={()=>this.markAsRead(note, 2)} note={note} visible={visible}></NoteItem>
-            )}
+          <NoteItem key={note.id} onClick={() => this.markAsRead(note, 2)} note={note} visible={visible}></NoteItem>
+        )}
       </div>
     );
   }
