3178914967c2bf549f5587782b29ec1d24842631
mark <komapc@gmail.com>
2020-05-21 18:10:27 +0300

    split more steps

--- client/src/components/meals/CreateMeal/TimeStep.js
+++ client/src/components/meals/CreateMeal/TimeStep.js
@@ -12,58 +12,60 @@ import locationIcon from "../../../resources/location_icon.svg"
 import dateIcon from "../../../resources/date_time_icon.svg"
 import servingsIcon from "../../../resources/servings_icon.svg"
 const TimeStep = props => {
-  
+
   const [state, updateState] = useState({
   });
 
   const update = (id, value) => {
     alert(id, value);
-    props.update({"id":id, "value":value});
+    props.update({ "id": id, "value": value });
   };
 
 
   return (
+    <div className="wizard-container">
       <div className="date-div">
-      {/* <span><img className="meal-info-icons" src={dateIcon} alt="date" /></span> */}
-      <span>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <Grid container justify="space-around"><span><img className="meal-info-icons"
-            src={dateIcon} alt="date" /></span>
-            <KeyboardDatePicker
-              variant="dialog"
-              ampm={false}
-              label="date"
-              id="date"
-              value={props.selectedDate}
-              //onChange={(value) => { updateState({ selectedDate: value }) }}
-              onChange={update}
-              onError={console.log}
-              disablePast
-              showTodayButton
-              autoOk
-              format="yyyy/MM/dd"
-            />
-          </Grid>
+        {/* <span><img className="meal-info-icons" src={dateIcon} alt="date" /></span> */}
+        <span>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid container justify="space-around"><span><img className="meal-info-icons"
+              src={dateIcon} alt="date" /></span>
+              <KeyboardDatePicker
+                variant="dialog"
+                ampm={false}
+                label="date"
+                id="date"
+                value={props.selectedDate}
+                //onChange={(value) => { updateState({ selectedDate: value }) }}
+                onChange={update}
+                onError={console.log}
+                disablePast
+                showTodayButton
+                autoOk
+                format="yyyy/MM/dd"
+              />
+            </Grid>
 
-          <Grid container justify="space-around"><span><img className="meal-info-icons"
-            src={dateIcon} alt="date" /></span>
-            <KeyboardTimePicker
-              variant="dialog"
-              ampm={false}
-              label="date & time"
-              id="time"
-              value={state.selectedDate}
-              //onChange={(value) => { updateState({ selectedDate: value }) }}
-              onChange={(e)=>{update("id", e)}}              
-              onError={console.log}
-              disablePast
-              showTodayButton
-              autoOk
-              format="HH:mm"
-            />
-          </Grid>
-        </MuiPickersUtilsProvider>
-      </span>
+            <Grid container justify="space-around"><span><img className="meal-info-icons"
+              src={dateIcon} alt="date" /></span>
+              <KeyboardTimePicker
+                variant="dialog"
+                ampm={false}
+                label="date & time"
+                id="time"
+                value={state.selectedDate}
+                //onChange={(value) => { updateState({ selectedDate: value }) }}
+                onChange={(e) => { update("id", e) }}
+                onError={console.log}
+                disablePast
+                showTodayButton
+                autoOk
+                format="HH:mm"
+              />
+            </Grid>
+          </MuiPickersUtilsProvider>
+        </span>
+      </div>
     </div>
   );
 };
