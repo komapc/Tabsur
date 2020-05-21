f623ea441d7f51635a8fd4352df1984cf453ee89
mark <komapc@gmail.com>
2020-05-21 18:15:31 +0300

    more steps

--- client/src/components/meals/CreateMeal/CreateMealWizard.js
+++ client/src/components/meals/CreateMeal/CreateMealWizard.js
@@ -121,21 +121,3 @@ const GuestStep = props => {
 };
 
 
-const ImageStep = props => {
-  const update = (e) => {
-    props.update(e.target);
-  };
-
-  const submit = (e) => {
-    props.submit(e);
-  };
-
-  return (
-    <div className="wizard-container">
-      <label>Upload image</label>
-      <input type='text' className='form-control' id="image"
-        onChange={(e) => { update(e) }} />
-      <button onClick={(e) => { submit(e) }}>submit</button>
-    </div>
-  );
-};
