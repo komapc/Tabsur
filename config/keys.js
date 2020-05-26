93f09795c5f062d9eae265e119bd69a1c366e07c
mark <komapc@gmail.com>
2020-05-26 11:39:15 +0300

    S3: fixes

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,6 @@ module.exports = {
     secretOrKey: "Dzirciema93",
     google: "AIzaSyBxcuGXRxmHIsiI6tDQDVWIgtGkU-CHZ-4",
     facebook_id:"441252456797282",
-    AWS_KEY:"AKIAIEHBGMJGB6L7LL7Q"
+    AWS_KEY:"AKIAIEHBGMJGB6L7LL7Q",
+    S3_BUCKET:"dining.philosophers.com"
 };
