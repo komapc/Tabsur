40dc07eba0bf5e32645b2968077c02bf727deb11
mark <komapc@gmail.com>
2020-05-26 23:37:54 +0300

    fix S3_BUCKET in keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,5 +3,5 @@ module.exports = {
     google: "AIzaSyBxcuGXRxmHIsiI6tDQDVWIgtGkU-CHZ-4",
     facebook_id:"441252456797282",
     AWS_KEY:"AKIAIEHBGMJGB6L7LL7Q",
-    S3_BUCKET:"dining.philosophers.com"
+    S3_BUCKET:"images.dining.philosophers.com"
 };
