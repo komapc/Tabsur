13e633990f0073ee67b8905ff94c23bda3b76650
mark <komapc@gmail.com>
2020-05-27 14:05:07 +0300

    format

--- client/src/components/meals/CreateMeal/NameStep.js
+++ client/src/components/meals/CreateMeal/NameStep.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 const NameStep = props => {
   const update = (e) => {
-    props.update(  e.target );
+    props.update(e.target);
   };
 
   return (
     <div className="wizard-container">
-    
-        <label>Meal Name</label>
-        <input type='text' className='form-control' id="name"
-          onChange={update} value={props.form.name}/>
 
-        <label>Description</label>
-        <div>
-        <textarea className='wizard-description' id="description" 
+      <label>Meal Name</label>
+      <input type='text' className='form-control' id="name"
+        onChange={update} value={props.form.name} />
+
+      <label>Description</label>
+      <div>
+        <textarea className='wizard-description' id="description"
           onChange={update} value={props.form.description} rows="5"
-          placeholder="Describe the meal"/>
-        </div>
+          placeholder="Describe the meal" />
+      </div>
     </div>
   );
 };
