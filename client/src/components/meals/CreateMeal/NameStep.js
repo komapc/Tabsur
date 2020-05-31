09d70bdaf05ba5028cc13b6ba9ca6c1b5a07ce90
mark <komapc@gmail.com>
2020-06-01 00:24:59 +0300

    attempts to alight arrows ans  other stuff in the wizard

--- client/src/components/meals/CreateMeal/NameStep.js
+++ client/src/components/meals/CreateMeal/NameStep.js
@@ -6,19 +6,21 @@ const NameStep = props => {
   };
 
   return (
+    <span className="location-input-field input-field col s12 ">
     <div className="wizard-container">
+        <label>Meal Name</label>
+        <input type='text' className='form-control' id="name"
+          onChange={update} value={props.form.name} />
 
-      <label>Meal Name</label>
-      <input type='text' className='form-control' id="name"
-        onChange={update} value={props.form.name} />
-
-      <label>Description</label>
-      <div>
-        <textarea className='wizard-description' id="description"
-          onChange={update} value={props.form.description} rows="5"
-          placeholder="Describe the meal" />
-      </div>
+        <label>Description</label>
+        <div>
+          <textarea className='wizard-description' id="description"
+            onChange={update} value={props.form.description} rows="5"
+            placeholder="Describe the meal" />
+        </div>
     </div>
+      </span>
+
   );
 };
 export default NameStep;
