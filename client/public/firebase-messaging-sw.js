8807e954623cb19f33af9826e55186875074a3d2
zanooda <zanooda@ya.ru>
2020-07-02 01:28:53 +0300

    remove unneeded, add measurementId

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
 firebase.initializeApp({
-// Project Settings => Add Firebase to your web app
   messagingSenderId: "156567484209"
 });
 const messaging = firebase.messaging();
--- client/src/init-fcm.js
+++ client/src/init-fcm.js
@@ -1,17 +1,17 @@
 import * as firebase from "firebase/app";
 import "firebase/messaging";
 const initializedFirebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBxcuGXRxmHIsiI6tDQDVWIgtGkU-CHZ-4",
-    authDomain: "tabsur.firebaseapp.com",
-    databaseURL: "https://tabsur.firebaseio.com",
-    projectId: "tabsur",
-    storageBucket: "tabsur.appspot.com",
-    messagingSenderId: "156567484209",
-    appId: "1:156567484209:web:811366754f1a296b482210"
+  apiKey: "AIzaSyBxcuGXRxmHIsiI6tDQDVWIgtGkU-CHZ-4",
+  authDomain: "tabsur.firebaseapp.com",
+  databaseURL: "https://tabsur.firebaseio.com",
+  projectId: "tabsur",
+  storageBucket: "tabsur.appspot.com",
+  messagingSenderId: "156567484209",
+  appId: "1:156567484209:web:811366754f1a296b482210",
+  measurementId: "G-TWDTEWH15M"
 });
 const messaging = initializedFirebaseApp.messaging();
 messaging.usePublicVapidKey(
-// Project Settings => Cloud Messaging => Web Push certificates
   "BNJJF1av86BIQPia6y1p4aqlPNRkH4C7IkExrREYb5xyr1EDpUAJtxMrVs0cpCeoIJjP2WEQGIC9FkKDamngxGc"
 );
 export { messaging };
