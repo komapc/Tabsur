582c9131c826647a5b11df248bfda9728a55e93b
zanooda <zanooda@ya.ru>
2020-07-01 14:43:55 +0300

    change  firebase-messaging-ws.js

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,35 +1,30 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
-
+importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
 firebase.initializeApp({
-  messagingSenderId: '358654109615' // !
+// Project Settings => Add Firebase to your web app
+  messagingSenderId: "358654109615"
 });
-
 const messaging = firebase.messaging();
-
-// Customize notification handler
 messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('Handling background message', JSON.stringify(payload));
-  // Copy data object to get parameters in the click handler
-  payload.data.data = JSON.parse(JSON.stringify(payload.data));
-  return self.registration.showNotification(payload.data.title, payload.data);
-});
-
-self.addEventListener('notificationclick', function(event) { // ?
-  const target = event.notification.data.click_action || '/';
-  event.notification.close();
-  // This looks to see if the current is already open and focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: 'window',
-    includeUncontrolled: true
-  }).then(function(clientList) {
-    // clientList always is empty?!
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url === target && 'focus' in client) {
-        return client.focus();
+  const promiseChain = clients
+    .matchAll({
+      type: "window",
+      includeUncontrolled: true
+    })
+    .then(windowClients => {
+      for (let i = 0; i < windowClients.length; i++) {
+        const windowClient = windowClients[i];
+        windowClient.postMessage(payload);
       }
-    }
-    return clients.openWindow(target);
-  }));
+    })
+    .then(() => {
+      return registration.showNotification("my notification title");
+    });
+  return promiseChain;
+});
+self.addEventListener('notificationclick', function(event) {
+  // do what you want
+  // ...
+
+  console.log('!!!', JSON.stringify(event));
 });
