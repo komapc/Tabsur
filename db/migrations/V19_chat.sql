064fb5e11b345e8c1f5da2d0e3a94224fbe9e129
mark <komapc@gmail.com>
2020-07-08 23:20:20 +0300

    fix create table

--- db/migrations/V19_chat.sql
+++ db/migrations/V19_chat.sql
@@ -4,7 +4,7 @@ CREATE TABLE messages
 	receiver integer NOT NULL,
  	sent timestamp with time zone DEFAULT now(),
 	status integer DEFAULT 0 --0=new, 1=received, 2=seen
-)
+);
 
 
 GRANT ALL ON TABLE messages TO PUBLIC;
