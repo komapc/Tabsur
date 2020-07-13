1bb58a82311220bacf0c8632805b07044e8b67a7
mark <komapc@gmail.com>
2020-07-13 17:37:47 +0300

    MessagesActions.js fixes

--- client/src/actions/MessagesActions.js
+++ client/src/actions/MessagesActions.js
@@ -2,15 +2,14 @@ import {
   SET_MESSAGES_COUNT
 } from "./types";
 
-function setMessagesCount(state, value) {
+export default function setMessagesCount(state, action) {
   switch (action.type) {
     case SET_MESSAGES_COUNT:
-      return Object.assign({}, state, {
-        messagesCount: value
+      return Object.assign({}, action, {
+        messagesCount: action.value
       })
     default:
       return state
   }
 }
 
-export default setMessagesCount;
