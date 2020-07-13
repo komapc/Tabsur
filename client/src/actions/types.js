8c021a2b6b38c41417c70ee53aa79f28547a24a4
mark <komapc@gmail.com>
2020-07-13 17:38:39 +0300

    add reducers types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,6 @@ export const GET_ERRORS = "GET_ERRORS";
 export const USER_LOADING = "USER_LOADING";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const GET_CURRENT_USER = "GET_CURRENT_USER";
+
+export const SET_MESSAGES_COUNT = "SET_MESSAGES_COUNT";
+export const SET_LOADING = "SET_LOADING";
