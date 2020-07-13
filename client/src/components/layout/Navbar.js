1bdf3cb8ab0eae0a3e753423923a704d3ed893b0
mark <komapc@gmail.com>
2020-07-13 18:06:32 +0300

    preparation for redux

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,69 +1,85 @@
 import React, { Component } from "react";
-import sandwich from "../../resources/menu.svg" 
-import notification from "../../resources/notification.svg" 
-import MessageOutlinedIcon from '@material-ui/icons/Message';
+import sandwich from "../../resources/menu.svg"
+import notification from "../../resources/notification.svg"
+import setMessagesCount from "../../actions/MessagesActions"
 import Menu from "./Menu.js"
 import Notifications from "./Notifications";
 import Badge from '@material-ui/core/Badge';
+import { connect } from "react-redux";
+import MessageOutlinedIcon from '@material-ui/icons/Message';
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
       hideNavBar: false,
-      visible:false,
+      visible: false,
       showMenu: false,
-      showNotifications: false
+      showNotifications: false,
+      messagesCount: this.props.messagesCount,
+      notificationCount: this.props.notificationCount
     };
   }
 
   openMenu = () => {
-    this.setState({showMenu: true});
+    this.setState({ showMenu: true });
   }
 
   openNotifications = () => {
-    this.props.setNewNotificationsCounter(0);
-    this.setState({showNotifications: true});
+    this.props.setMessagesCount({});
+   //this.props.setNewNotificationsCounter(0);
+    this.setState({ showNotifications: true });
   }
 
   openMessages = () => {
-    this.props.setNewMessagesCounter(0);
+    //this.props.setNewMessagesCounter(0);
     alert('messages under construction');
-  }
+  };
 
   render() {
     return (
       <div className="navbar-top">
         <div>
-          <div>
-            <span onClick={this.openMessages}>
-              <Badge badgeContent={this.props.newMessagesCounter} color="secondary">
-                <MessageOutlinedIcon fontSize="large" color="disabled" alt={"Messages"}/>
-              </Badge>
-            </span>
-            <span onClick={this.openNotifications} >
-              <Badge badgeContent={this.props.newNotificationsCounter} color="secondary">
-                <img className="navbar-icons" src={notification} alt={"Notifications"}/>
-              </Badge>
-            </span>
-            <span onClick={this.openMenu}>
-              <Badge badgeContent={0} color="secondary">
-                <img className="navbar-icons" src={sandwich} alt={"..."}/> 
-              </Badge>
-            </span>
-          </div>
+          <span onClick={this.openMessages}>
+            <Badge badgeContent={this.props.newMessagesCounter} color="secondary">
+              <MessageOutlinedIcon fontSize="large" color="disabled" alt={"Messages"} />
+            </Badge>
+          </span>
+          <span onClick={this.openNotifications} >
+            <Badge badgeContent={this.props.newNotificationsCounter} color="secondary">
+              <img className="navbar-icons" src={notification} alt={"Notifications"} />
+            </Badge>
+          </span>
+          <span onClick={this.openMenu}>
+            <Badge badgeContent={0} color="secondary">
+              <img className="navbar-icons" src={sandwich} alt={"..."} />
+            </Badge>
+          </span>
         </div>
-        <div> 
-          <Menu visible={this.state.showMenu} 
-            onItemClicked={()=>{this.setState({showMenu: false})}} />
-            
-          <Notifications 
-            visible={this.state.showNotifications} 
-            onItemClicked={()=>{this.setState({showNotifications: false})}} />
-         </div> 
+        <div>
+          <Menu visible={this.state.showMenu}
+            onItemClicked={() => { this.setState({ showMenu: false }) }} />
+
+          <Notifications
+            visible={this.state.showNotifications}
+            onItemClicked={() => { this.setState({ showNotifications: false }) }} />
+        </div>
       </div>
     );
   }
 }
 
-export default Navbar;
+const mapStateToProps = state => ({
+  notificationsCount: state.notificationsCount,
+  messagesCount: state.messagesCount
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setMessagesCount: () => dispatch(setMessagesCount()),
+    dispatch
+  }
+}
+export default connect(
+  mapStateToProps, mapDispatchToProps,
+)(Navbar);
