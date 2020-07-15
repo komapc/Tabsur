7c006e579bb8fe0a98c82156912bb3cbdf71029f
zanooda <zanooda@ya.ru>
2020-07-15 17:54:15 +0300

    use badges

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -41,16 +41,14 @@ class Navbar extends Component {
   render() {
     return (
       <div className="navbar-top">
-        
-      <div>{JSON.stringify(this.props.messagesCount)}</div>
         <div>
           <span onClick={this.openMessages}>
-            <Badge badgeContent={this.state.messagesCount} color="secondary">
+            <Badge badgeContent={this.props.messagesCount} color="secondary">
               <MessageOutlinedIcon fontSize="large" color="disabled" alt={"Messages"} />
             </Badge>
           </span>
           <span onClick={this.openNotifications} >
-            <Badge badgeContent={this.state.notificationsCount} color="secondary">
+            <Badge badgeContent={this.props.notificationsCount} color="secondary">
               <img className="navbar-icons" src={notification} alt={"Notifications"} />
             </Badge>
           </span>
