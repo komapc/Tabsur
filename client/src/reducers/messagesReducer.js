dfecb1f2293896e69c9c67432667de284c396af0
mark <komapc@gmail.com>
2020-07-15 15:07:19 +0300

    remove log

--- client/src/reducers/messagesReducer.js
+++ client/src/reducers/messagesReducer.js
@@ -3,7 +3,6 @@ import { SET_MESSAGES_COUNT } from "../actions/types";
 const initialState = 0;
 
 export default function(state = initialState, action) {
-  console.log("messages reducer: " + JSON.stringify(action)); 
   switch (action.type) {
     case SET_MESSAGES_COUNT:
       console.log("messages reducer: SET_MESSAGES_COUNT, " + JSON.stringify(action.messagesCount));
