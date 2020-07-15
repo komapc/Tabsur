a4c21480b4858fa52507d185556b1f102f5c9a01
mark <komapc@gmail.com>
2020-07-16 00:54:05 +0300

    push notification - separate function

--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -10,6 +10,118 @@ if (process.env.NODE_ENV === "debug")
   currentConfig = pgConfig.pgConfigLocal;
 }
 
+function pushNotification(notification, registration_ids)
+{
+  fcm.sendNotification(JSON.stringify({
+    data: notification,
+    "registration_ids": registration_ids //resp.rows[0].tokens.split(';')
+  })) 
+  .then(function(response) {
+    console.log(JSON.stringify(response.body()));
+    return response.json(response);
+  })
+  .catch(error)
+  {
+    console.log(`Error: ${JSON.stringify(error)}`);
+  }
+}
+
+const addNotificationToDB = async (message) =>
+{
+
+//example of param:
+// const message =
+// {
+//   title: 'Attend', 
+//   body:  'A user wants to join your meal', 
+//   icon: 'resources/Message-Bubble-icon.png', 
+//   click_action: '/Meals/',
+//   receiver: attend.user_id,
+//   meal_id:  attend.meal_id,
+//   sender: -1,
+//   type: 5
+// }
+
+
+
+  // const query = `
+  //   INSERT INTO messages (sender, receiver, status, message) 
+  //   VALUES ($1, $2, $3, $4) 
+  //   RETURNING 
+  //   id AS message_id,
+  //   (
+  //     SELECT array_to_string(array_agg(token),';') 
+  //     AS tokens 
+  //     FROM user_tokens
+  //     WHERE user_id=$5
+  //   )`;
+
+  // const query = `
+  // INSERT INTO notifications (meal_id, receiver, message_text, sender, note_type, 
+  //       click_action, icon, title) 
+  //     VALUES (
+  //       $1, 
+  //       (SELECT host_id FROM meals WHERE id=$1), 
+  //       CONCAT((SELECT name FROM users WHERE id=$2),' wants to join your meal.'),
+  //       0, 
+  //       5,
+  //       $3, $4, $5
+  //       )  
+  //     RETURNING (
+  //       SELECT array_to_string(array_agg(token),';') 
+  //       AS tokens 
+  //       FROM user_tokens
+  //       WHERE user_id=(SELECT host_id FROM meals WHERE id=$1)
+  //     ),
+  //     CONCAT((SELECT name FROM users WHERE id=$2), ' wants to join your meal.') AS body
+  //   `;
+
+   const query = `
+  INSERT INTO notifications (meal_id, receiver, message_text, sender, note_type, 
+        click_action, icon, title) 
+      VALUES (
+       $1, $2, $3, $4, $5, $6, $7, $8)
+
+      RETURNING (
+              SELECT array_to_string(array_agg(token),';') 
+              AS tokens 
+              FROM user_tokens
+              WHERE user_id=$2 
+            )
+    `;
+  const client = new Client(currentConfig);
+  await client.connect();
+  client.query(query,     
+    [message.meal_id, 
+      message.receiver,
+      message.body, 
+      message.sender, 
+      message.type,
+      message.click_action, 
+      message.icon,
+      message.title, 
+     ]
+      )
+  .then(resp => {
+    console.log(`Message id: ${resp.rows[0].message_id} inserted sucssesfuly`);
+    return resp;
+  })
+  .catch(error => {
+    console.error(error); 
+    return response.status(500).json(error); 
+  })
+  .finally(() => {
+    client.end();
+  });
+}
+//add notificatin/message to the DB + push 
+function addNotification(notification)
+{
+  const  resp =  addNotificationToDB(notification);
+  pushNotification(notification, resp);
+}
+
+
 // @route GET api/notifications
 // @desc get list og user's notifications
 // @access Public
@@ -98,44 +210,23 @@ router.post("/send-message", async (req, response)=> {
   console.log(`Message: ${req.body.message} from ${req.body.sender} to ${req.body.receiver}`);
 
   const client = new Client(currentConfig);
-  const query = `
-    INSERT INTO messages (sender, receiver, status, message) 
-    VALUES ($1, $2, $3, $4) 
-    RETURNING 
-    id AS message_id,
-    (
-      SELECT array_to_string(array_agg(token),';') 
-      AS tokens 
-      FROM user_tokens
-      WHERE user_id=$5
-    )`;
-  await client.connect();
-  client.query(query, [req.body.sender, req.body.receiver, 0, req.body.message, req.body.receiver])
-  .then(resp => {
-    console.log(`Message id: ${resp.rows[0].message_id} inserted sucssesfuly`);
-    fcm.sendNotification(JSON.stringify({
-      data: {
-          title: 'Message', 
-          type: 'message',
-          message_id: resp.rows[0].message_id,
-          body:  req.body.message, 
-          icon: 'https://icons.iconarchive.com/icons/pelfusion/long-shadow-media/128/Message-Bubble-icon.png', 
-          click_action: 'http://info.cern.ch/hypertext/WWW/TheProject.html'
-      },
-      "registration_ids": resp.rows[0].tokens.split(';')
-    }))
-    .then(function(response) {
-      console.log(JSON.stringify(response));
-      return response.json(resp.rows);
-    })
-  })
-  .catch(error => {
-    console.error(error); 
-    return response.status(500).json(error); 
-  })
-  .finally(() => {
-    client.end();
-  });
+
+  const message =
+  {
+    title: 'Message', 
+    body: req.body.message, 
+    icon: 'resources/Message-Bubble-icon.png', 
+    click_action: '/User/req.body.receiver',
+    receiver: req.body.receiver,
+    meal_id:  -1,
+    sender: req.body.sender,
+    type: 0
+  }
+
+
+  addNotification(message);
 });
 
 module.exports = router;
+
+exports.addNotification = addNotification;
