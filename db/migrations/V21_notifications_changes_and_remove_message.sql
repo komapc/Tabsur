7668843235af76b454ff4fd41a0ace1d5e610ef1
mark <komapc@gmail.com>
2020-07-16 00:53:08 +0300

    fx table: notifications

--- db/migrations/V21_notifications_changes_and_remove_message.sql
+++ db/migrations/V21_notifications_changes_and_remove_message.sql
@@ -2,4 +2,4 @@ alter table notifications rename  column user_id to receiver;
 
 alter table notifications add  column click_action varchar(100);
 alter table notifications add  column icon varchar(100);
-alter table title add  column icon varchar(100);
+alter table notifications add  column title varchar(100);
