664a11316cd57f6ed166045b465d2b30fe9cb4cc
mark <komapc@gmail.com>
2020-07-28 14:46:15 +0300

    getChatMessages

--- client/src/components/chat/ChatUser.js
+++ client/src/components/chat/ChatUser.js
@@ -1,11 +1,12 @@
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import React from "react";
+import { getChatUsers } from "../../actions/chatActions";
 class ChatUser extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      meal: props.meal
+      user: props.user
     };
   }
 
@@ -13,6 +14,21 @@ class ChatUser extends React.Component {
   render() {
   return <span>
     chat with a user 
+
+    <div>
+          {
+            this.props.auth.user.id !== this.state.id ?
+              <div>
+                <input type="text" id="message" placeholder="Message"></input>
+                <button onClick={() => this.sendMessageWithCallback(
+                  this.props.auth.user.id,
+                  this.state.id,
+                  document.getElementById("message").value
+                )}>Send</button>
+              </div> :
+              <div></div>
+          }
+        </div>
   </span>
 };
  
