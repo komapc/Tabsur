459d05c3e14b8f931356342ea8c24881cd665d02
mark <komapc@gmail.com>
2020-08-02 22:41:16 +0300

    fixes in scrolling

--- client/src/components/layout/Bottom.js
+++ client/src/components/layout/Bottom.js
@@ -5,29 +5,31 @@ import list from "../../resources/bottom_menu/list_bar.svg"
 import plus from "../../resources/bottom_menu/add_meal_bar.svg"
 import myMeals from "../../resources/bottom_menu/my_meals_bar.svg"
 
-class Bottom extends Component {
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+class  Bottom extends Component {
   constructor() {
     super();
-    this.state = {
-      url: window.location.pathname
-    };
+
   }
 
   render() {
     return (
 
       <div className="footer">
-        <nav>
-          <NavLink to="/MealMap" activeClassName="active">
-            <img className="footer-icons" src={map} alt={"meals map"} /></NavLink >
-          <NavLink to="/Meals" activeClassName="active">
-            <img className="footer-icons" src={list} alt={"meals list"} /> </NavLink >
-          <NavLink to="/createMealWizard" activeClassName="active">
-            <img className="footer-icons" src={plus} alt={"add meal"} /></NavLink >
-          <NavLink to="/MyMeals" activeClassName="active">
-            <img className="footer-icons" src={myMeals} alt={"my meals"} /> </NavLink >
-
-        </nav>
+        <Tabs value={this.props.index}  onChange={this.props.onChange}  selectedIndex={this.props.index} 
+        fullWidth centered selectionFollowsFocus='True' indicatorColor='primary'
+        inkBarStyle={{background: 'Black'}} 
+        TabIndicatorProps={{
+          style: {
+            backgroundColor: "#D97D54"
+          }
+        }}>
+            <Tab label="Meals" icon={<img className="footer-icons" src={map} alt={"meals map"} />}> </Tab>
+            <Tab label="My Profile"  icon={<img className="footer-icons" src={list} alt={"meals map"} />}></Tab>  
+            <Tab label="My Meals"  icon={<img className="footer-icons" src={plus} alt={"meals map"} />}></Tab>
+            <Tab label="Add Meal" icon={<img className="footer-icons" src={myMeals} alt={"meals map"} />}></Tab>
+          </Tabs>
 
       </div>
     );
