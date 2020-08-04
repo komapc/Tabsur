4c55a01d4a894e1b5ecfd531cdc072573602f81f
zanooda <zanooda@ya.ru>
2020-08-04 15:58:53 +0300

    fix bottom

--- client/src/components/layout/Bottom.js
+++ client/src/components/layout/Bottom.js
@@ -1,36 +1,27 @@
 import React, { Component } from "react";
-import { NavLink } from 'react-router-dom';
 import map from "../../resources/bottom_menu/map_bar.svg"
 import list from "../../resources/bottom_menu/list_bar.svg"
 import plus from "../../resources/bottom_menu/add_meal_bar.svg"
 import myMeals from "../../resources/bottom_menu/my_meals_bar.svg"
-
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 class  Bottom extends Component {
-  constructor() {
-    super();
-
-  }
-
   render() {
     return (
-
       <div className="footer">
         <Tabs value={this.props.index}  onChange={this.props.onChange}  selectedIndex={this.props.index} 
-        fullWidth centered selectionFollowsFocus='True' indicatorColor='primary'
-        inkBarStyle={{background: 'Black'}} 
-        TabIndicatorProps={{
-          style: {
-            backgroundColor: "#D97D54"
-          }
+          fullWidth centered selectionFollowsFocus='True' indicatorColor='primary'
+          inkBarStyle={{background: 'Black'}} 
+          TabIndicatorProps={{
+            style: {
+              backgroundColor: "#D97D54"
+            }
         }}>
-            <Tab label="Meals" icon={<img className="footer-icons" src={map} alt={"meals map"} />}> </Tab>
-            <Tab label="My Profile"  icon={<img className="footer-icons" src={list} alt={"meals map"} />}></Tab>  
-            <Tab label="My Meals"  icon={<img className="footer-icons" src={plus} alt={"meals map"} />}></Tab>
-            <Tab label="Add Meal" icon={<img className="footer-icons" src={myMeals} alt={"meals map"} />}></Tab>
-          </Tabs>
-
+          <Tab label="Meals" icon={<img className="footer-icons" src={map} alt={"meals map"} />}> </Tab>
+          <Tab label="My Profile"  icon={<img className="footer-icons" src={list} alt={"meals map"} />}></Tab>  
+          <Tab label="My Meals"  icon={<img className="footer-icons" src={myMeals} alt={"meals map"} />}></Tab>
+          <Tab label="Add Meal" icon={<img className="footer-icons" src={plus} alt={"meals map"} />}></Tab>
+        </Tabs>
       </div>
     );
   }
