84d73377cd8313ebf48b4674e00ef87a3851346a
mark <komapc@gmail.com>
2020-08-06 19:25:55 +0300

    import Button from '@material-ui/core/Button';

--- client/src/components/auth/MyProfile.js
+++ client/src/components/auth/MyProfile.js
@@ -10,7 +10,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-
+import Button from '@material-ui/core/Button';
 import tmpBgImg from "../../resources/images/susi.jpeg";
 import { makeStyles } from '@material-ui/core/styles';
 
