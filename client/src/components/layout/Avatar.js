cc14841b32075271879a5964d14f41a81e349db4
zanooda <zanooda@ya.ru>
2020-08-06 18:36:47 +0300

    remove template images from MyProfile

--- client/src/components/layout/Avatar.js
+++ client/src/components/layout/Avatar.js
@@ -22,16 +22,17 @@ const useStyles = makeStyles(theme => ({
         height: theme.spacing(17),
         borderWidth: theme.spacing(1), 
         borderColor: 'white', 
-        borderStyle:'solid'
+        borderStyle:'solid',
+        backgroundColor: '#13A049'
     }
 }));
 
 const Avatar = () => {
     const classes = useStyles();
-  
+    const img = null; // tmpAvatarImg
     return (
         <div className={classes.root}>
-            <MaterialUiAvatar alt="Avatar alt string" src={tmpAvatarImg} className={classes.large} />
+            <MaterialUiAvatar alt="Avatar alt string" src={img} className={classes.large} />
         </div>
     );
 }
