9ca877202d1a143a8365b144e115e1dc5e5e6412
mark <komapc@gmail.com>
2020-08-07 19:24:16 +0300

    NameStep: margins + material UI

--- client/src/components/meals/CreateMeal/NameStep.js
+++ client/src/components/meals/CreateMeal/NameStep.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import { Input, InputLabel, TextField  } from '@material-ui/core';
-
+import { TextField, Grid, Box } from '@material-ui/core';
 const NameStep = props => {
   const update = (e) => {
     props.update(e.target);
   };
 
   return (
-    <span className="location-input-field input-field col s12 ">
-      <div className="wizard-container">
-        <InputLabel>Meal Name</InputLabel>
-        <Input type='text' className='form-control' id="name"
-          onChange={update} value={props.form.name} />
+    <div className="wizard-container row ">
 
-        <InputLabel>Description</InputLabel>
-        <div>
-          <TextField  className='wizard-description' id="description"
-            onChange={update} value={props.form.description} rows="5"
-            placeholder="Describe the meal" />
-        </div>
-      </div>
-    </span>
+      <Grid container spacing={4}>
+        <Box  item  m={2} p={3}>
+          <TextField
+            className='wizard-description  justify-content-center' id="name" row
+            onChange={update} value={props.form.name} label="Meal Name" />
+
+        </Box >
+        <Box  item  m={2} p={3}>
+          <TextField className='wizard-description' id="description" row 
+            onChange={update} value={props.form.description}
+            placeholder="Describe the meal" label="Description" />
+        </Box >
+      </Grid>
+    </div>
 
   );
 };
