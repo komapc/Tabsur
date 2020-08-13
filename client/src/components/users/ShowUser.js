4e22ae1889dbcb143742cfaa9aad4c5ec3e62645
zanooda <zanooda@ya.ru>
2020-08-13 16:10:57 +0300

    work on ShowUser.js

--- client/src/components/users/ShowUser.js
+++ client/src/components/users/ShowUser.js
@@ -4,9 +4,214 @@ import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import backButton from "../../resources/back_button.svg";
 import defaultImage from "../../resources/userpic_empty.svg";
+import TextField from '@material-ui/core/TextField';
 
 import { getFollowStatus, setFollow, getUserInfo } from "../../actions/userActions"
 import { sendMessage } from "../../actions/notifications"
+
+
+import PropTypes from "prop-types";
+import { registerUser, getUser } from "../../actions/authActions";
+import classnames from "classnames";
+import Avatar from "../layout/Avatar"
+import Grid from '@material-ui/core/Grid';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import tmpBgImg from "../../resources/images/susi.jpeg";
+import { makeStyles } from '@material-ui/core/styles';
+import { logoutUser } from "../../actions/authActions";
+import store from "../../store";
+
+//#region ProfileHeader
+const useStylesHeader = makeStyles(theme => ({
+  alignItemsAndJustifyContent: {
+    width: "100%",
+    height: theme.spacing(25),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#13A049',
+    //backgroundImage: `url(${tmpBgImg})`,
+    backgroundSize: 'cover',
+  },
+  empty: {
+    height: 64
+  },
+  wrapper: {
+    marginTop: '190px'
+  }
+}))
+const ProfileHeader = () => {
+  const classes = useStylesHeader()
+  return (
+    <React.Fragment>
+      <div className={classes.alignItemsAndJustifyContent}>
+        <div className={classes.wrapper}>
+          <Avatar />
+        </div>
+      </div>
+      <div className={classes.empty}></div>
+    </React.Fragment>
+  )
+}
+//#endregion
+
+
+//#region ProfileStats
+const useStylesStats = makeStyles(theme => ({
+  headerContainer: {
+    width: "100%",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  header: {
+    color: "#13A049",
+    fontSize: 32,
+    fontWeight: "fontWeightBold",
+    fontStyle: "bold",
+    //fontFamily: "Monospace"
+    margin: '20px'
+  },
+  stat: {
+    color: "#13A049",
+    fontSize: 16,
+    fontWeight: "fontWeightBold",
+    alignItems: 'left',
+    justifyContent: 'left',
+    display: 'flex',
+    paddingLeft: '30%'
+  }
+}))
+
+const ProfileStats = (props) => {
+  
+  const classes = useStylesStats();
+  const userStats=props.userStats;// ? props.userStats[0] : {}; ???
+  console.log(`ProfileStats's props: ${JSON.stringify(props)}`);
+
+   return  <React.Fragment>
+      {/* {userStats?<div>{ JSON.stringify(userStats)}</div>:<span/>} */}
+      <div className={classes.headerContainer}>
+        <h5 className={classes.header}>{props.name}</h5>
+      </div>
+      <div className={classes.headerContainer}>
+        <Grid container >
+          <Grid item xs={6}><span className={classes.stat}>Followers _</span></Grid>
+          <Grid item xs={6}><span className={classes.stat}>Active meals _</span></Grid>
+          <Grid item xs={6}><span className={classes.stat}>Following _</span></Grid>
+          <Grid item xs={6}><span className={classes.stat}>Meals Created {userStats.meals_created}</span></Grid>
+        </Grid>
+      </div>
+    </React.Fragment>
+  
+}
+//#endregion
+
+//#region ProfileTabs
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+const useStylesTabs = makeStyles(theme => ({
+  root: {
+    flexGrow: 1, // ?
+
+    color: "#13A049",
+    fontSize: 16,
+    fontWeight: "fontWeightBold",
+    width: "100%",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomTab: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
+  }
+}));
+const ProfileTabs = (props) => {
+  const classes = useStylesTabs();
+ 
+  const [value, setValue] = React.useState(0); 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <React.Fragment>
+      <div className={classes.root}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          centered
+          indicatorColor='primary'
+          TabIndicatorProps={{ style: { backgroundColor: "primary"}}}>
+          <Tab label="Kitchen" {...a11yProps(0)} />
+          <Tab label="Gallery" {...a11yProps(1)} />
+        </Tabs>
+      </div>
+      <TabPanel value={value} index={0} >
+        {/* <div className='centered'> */}
+        {
+          props.followStatus ?
+          <Button variant="contained" color="primary" onClick={() => props.follow(0)}>UnFollow</Button> :
+          <Button variant="contained" color="primary" onClick={() => props.follow(3)}>Follow</Button>
+        }
+        {/* </div> */}
+
+        {
+            props.auth.user.id !== props.state.id ?
+              <div>
+                <TextField type="text" id="message" placeholder="Message"></TextField>
+                <Button 
+                  variant="contained" color="primary"
+                  onClick={() => props.sendMessageWithCallback(
+                    props.auth.user.id,
+                    props.state.id,
+                    document.getElementById("message").value
+                  )}>Send</Button>
+              </div> : null
+        }
+
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Under Construction
+      </TabPanel>
+    </React.Fragment>
+  )
+}
+//#endregion
+
 class ShowUser extends Component {
 
   constructor(props) {
@@ -78,7 +283,21 @@ class ShowUser extends Component {
   }
 
   render() {
+    console.log(`this.state.user: ${JSON.stringify(this.state.user)}`);
+    //let userStats = {}
+
     return (
+      <React.Fragment>
+
+        {false ? ( <React.Fragment>
+
+        <ProfileHeader /> {/* TODO: Pass avatar img or use Redux. Avatar image not implemented */}
+        <ProfileStats name={this.state.user.name}  userStats={{ meals_created: this.state.user.meals_created }} />
+        <ProfileTabs followStatus={this.state.followStatus} follow={this.follow} auth={this.props.auth} state={this.state} sendMessageWithCallback={this.sendMessageWithCallback}/>
+
+        </React.Fragment>) : null}
+
+      {true ? (
       <div className="info-all">
         <div className="info-back-div"><img
           className="info-back"
@@ -122,6 +341,8 @@ class ShowUser extends Component {
           }
         </div>
       </div>
+      ) : null}
+      </React.Fragment>
     );
   }
 }
