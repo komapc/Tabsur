aa40ee1754da816bb2a880674184b7abf22a1e59
zanooda <zanooda@ya.ru>
2020-08-13 13:42:55 +0300

    move ShowUser.js to ../users

--- client/src/components/users/Profile.js
+++ client/src/components/users/Profile.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getUser } from "../../actions/authActions";
 
+// TODO: remove this file
 class Profile extends Component {
   constructor(props) {
     super(props);
