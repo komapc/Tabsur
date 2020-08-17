6f9f087cc13e62743bd739af6399f09b55a3c467
mark <komapc@gmail.com>
2020-08-17 15:02:01 +0300

    change icons

--- client/src/components/meals/MealMapShow.js
+++ client/src/components/meals/MealMapShow.js
@@ -1,32 +1,45 @@
 import React from "react";
 import Geocode from "react-geocode";
 import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps";
-import attended from "../../resources/attended.svg";
-import fullUp from "../../resources/full_meal_icon.svg";
-import hosted from "../../resources/my_meal_icon.svg"
-import available from "../../resources/active_meal_icon.svg"
-import touched from "../../resources/touched_meal.svg"
+
+import attended_u from "../../resources/attended_meal_icon.svg";
+import fullUp_u from "../../resources/full_meal_icon.svg";
+import hosted_u from "../../resources/my_meal_icon.svg"
+import available_u from "../../resources/active_meal_icon.svg"
+
+import attended_t from "../../resources/attended_meal_icon_touched.svg";
+import fullUp_t from "../../resources/full_meal_icon_touched.svg";
+import hosted_t from "../../resources/my_meal_icon_touched.svg"
+import available_t from "../../resources/active_meal_icon_touched.svg"
+
+const attended = [attended_u, attended_t]
+const fullUp = [fullUp_u, fullUp_t]
+const hosted = [hosted_u, hosted_t]
+const available = [available_u, available_t]
+
+//import touched from "../../resources/touched_meal.svg"
 
 export const GOOGLE_MAPS_API_KEY = "AIzaSyBxcuGXRxmHIsiI6tDQDVWIgtGkU-CHZ-4";
 
 Geocode.setApiKey(GOOGLE_MAPS_API_KEY);
-const MealMapShow = React.memo(({ meals, defaultLocation, onMarkerClick, onMapClick, userId, selectedMeal }) => {
+const MealMapShow = 
+    React.memo(({ meals, defaultLocation, onMarkerClick, onMapClick, userId, selectedMeal }) => {
     const getMealIcon = (meal, userId, selectedMeal) => {
-        console.log(JSON.stringify(meal));
-        console.log(selectedMeal);
-       
+        console.log(`meal: ${JSON.stringify(meal)}`);
+        console.log(`selectedMeal = ${JSON.stringify(selectedMeal)}`);
+        const isSelected =  meal.id === selectedMeal?1:0;
+      
         if (meal.guest_count <= meal.Atendee_count) {
-            return fullUp;
+            return fullUp[isSelected];
         }
 
         if (meal.host_id === userId) {
-            return hosted;
+            return hosted[isSelected];
         }
         if (meal.me > 0 ) {
-          return attended;
+          return attended[isSelected];
         }
-        return (meal.id === selectedMeal)?
-             touched: available;
+        return available[isSelected];
     }
     // const shouldComponentUpdate = (nextProps,nextState) =>{
     //     return false;
