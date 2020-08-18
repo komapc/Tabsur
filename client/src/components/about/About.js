1a7dc8f587ccc52a73cb1c0367502a753866088c
mark <komapc@gmail.com>
2020-08-18 15:57:50 +0300

    revert mistaken commit

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,117 +1,46 @@
-/* global google */
 import React, { Component } from "react";
-import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
-import PlacesAutocomplete from "react-places-autocomplete";
+import { connect } from "react-redux";
+import { system } from "../../actions/authActions";
+class About extends Component {
 
-class LocationSearchInput extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state = { address: "" };
-    this.placesService = new window.google.maps.places.PlacesService(
-      document.createElement("div")
-    );
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
+    this.state = {
+      user: []
+    };
+    system().then(systemData => {
+         console.log(systemData);
+    })
   }
 
-  handleChange = (address) => {
-    this.setState({ address });
-  };
-
-  handleSelect = (address, placeId) => {
-    this.setState({ address });
-
-    const request = {
-      placeId: placeId,
-      fields: ["name", "geometry"]
-    };
-    this.placesService.getDetails(request, (place, status) => {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        this.props.onPlaceChanged(place);
-      }
-    });
-  };
-
   render() {
+    const user = (this.props.auth.isAuthenticated )? this.props.auth.user.name:" [anonymous]";
+    console.log(`About log: ${user}; auth: ${JSON.stringify(this.props.auth)}`);
     return (
-      <PlacesAutocomplete
-        value={this.state.address}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div className="autocomplete-container">
-            <input
-              {...getInputProps({
-                placeholder: "Search Places ...",
-                className: "location-search-input"
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map((suggestion) => {
-                const className = suggestion.active
-                  ? "suggestion-item--active"
-                  : "suggestion-item";
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                  ? { backgroundColor: "#fafafa", cursor: "pointer" }
-                  : { backgroundColor: "#ffffff", cursor: "pointer" };
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style
-                    })}
-                  >
-                    <span>{suggestion.description}</span>
-                  </div>
-                );
-              })}
+      <div className="main">
+          <div className="landing-copy ">
+            <h4>  Hey, 
+              <small>{user}</small>
+            </h4>
+            <div>
+              Welcome to Coolanu TableSurfing community!
+              <br />
+              If you have food &ndash; invite guests,
+              <br />
+              if you are hungry &ndash; find a host!
             </div>
           </div>
-        )}
-      </PlacesAutocomplete>
-    );
-  }
-}
-
-class MapContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { places: [] };
-    this.showPlace = this.showPlace.bind(this);
-    this.mapRef = React.createRef();
-  }
-
-  showPlace(place) {
-    this.setState((prevState) => ({
-      places: [...prevState.places, place]
-    }));
-    this.mapRef.current.map.setCenter(place.geometry.location);
-  }
-
-  render() {
-    return (
-      <div className="map-container">
-        <LocationSearchInput onPlaceChanged={this.showPlace} />
-        <Map
-          ref={this.mapRef}
-          google={this.props.google}
-          className={"map"}
-          zoom={4}
-          initialCenter={this.props.center}
-        >
-          {this.state.places.map((place, i) => {
-            return <Marker key={i} position={place.geometry.location} />;
-          })}
-        </Map>
       </div>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyBxcuGXRxmHIsiI6tDQDVWIgtGkU-CHZ-4",
-  libraries: ["places"]
-})(MapContainer);
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+)(About);
