d1695a3f3804fa3f5ec983b3de01aeba52f39319
mark <komapc@gmail.com>
2020-09-09 14:39:50 +0300

    cosmetics

--- client/src/components/users/Friends.js
+++ client/src/components/users/Friends.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { getUserFollowees } from "../../actions/userActions"
+import Avatar from '@material-ui/core/Avatar';
 
-import config from "../../config";
-
+import CardHeader from '@material-ui/core/CardHeader';
 const Friend = (props) => {
-  return <div key={props.key}>
-  {props.name}
+  return <div 
+  style={{width: '100%', borderBottomColor: 'lightgray', borderBottomWidth: '1px', borderBlockEndStyle: 'solid'}}>
+   <CardHeader key={props.key} 
+    avatar={<Avatar aria-label="recipe" style={{backgroundColor: '#13A049'}}>
+      {props.name[0].toUpperCase()}
+    </Avatar>}
+  title={props.name}>
+</CardHeader>
 </div>
 }
 const Friends = (props) => {
