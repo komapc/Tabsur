1fa970578b65e85221d7c18089784b5820e8d6d4
mark <komapc@gmail.com>
2020-09-14 18:42:48 +0300

    "here" button fixes

--- client/src/components/meals/MealMapShow.js
+++ client/src/components/meals/MealMapShow.js
@@ -12,7 +12,8 @@ import attended_t from "../../resources/map/attended_meal_icon_touched.svg";
 import fullUp_t from "../../resources/map/full_meal_icon_touched.svg";
 import hosted_t from "../../resources/map/my_meal_icon_touched.svg"
 import available_t from "../../resources/map/active_meal_icon_touched.svg"
-import Button from '@material-ui/core/Button';
+
+import myLocation from "../../resources/map/my_location.svg"
 const attended = [attended_u, attended_t]
 const fullUp = [fullUp_u, fullUp_t]
 const hosted = [hosted_u, hosted_t]
@@ -21,105 +22,95 @@ const available = [available_u, available_t]
 export const GOOGLE_MAPS_API_KEY = "AIzaSyBxcuGXRxmHIsiI6tDQDVWIgtGkU-CHZ-4";
 
 Geocode.setApiKey(GOOGLE_MAPS_API_KEY);
-const MealMapShow = 
+const MealMapShow =
     React.memo(({ meals, defaultLocation, onMarkerClick, onMapClick, userId, selectedMeal }) => {
-    const getMealIcon = (meal, userId, isSelected) => {
-        const selectedIndex = isSelected?1:0;
-        console.log(`meal: ${JSON.stringify(meal)}`);
-        console.log(`selectedMeal = ${selectedIndex}`);
-      
-        if (meal.guest_count <= meal.Atendee_count) {
-            return fullUp[selectedIndex];
-        }
+        const getMealIcon = (meal, userId, isSelected) => {
+            const selectedIndex = isSelected ? 1 : 0;
+            console.log(`meal: ${JSON.stringify(meal)}`);
+            console.log(`selectedMeal = ${selectedIndex}`);
 
-        if (meal.host_id === userId) {
-            return hosted[selectedIndex];
-        }
-        if (meal.me > 0 ) {
-            return attended[selectedIndex];
-        }
-        return available[selectedIndex];
-    } 
-    const MyGoogleMap = (props) =>
-    {
-        const google=window.google;
-        const [position, setPosition] = useState({
-            lat: -25.0270548,
-            lng: 115.1824598
-          });
-        const myOptions = {
-            zoom: 2,
-            mapTypeControlOptions: {
-                //style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-                //position: google.maps.ControlPosition.TOP_CENTER,
+            if (meal.guest_count <= meal.Atendee_count) {
+                return fullUp[selectedIndex];
+            }
 
-              mapTypeIds: []
-            }, // here´s the array of controls
-            disableDefaultUI: true, // a way to quickly hide all controls
-            mapTypeControl: true,
-            scaleControl: true,
-            zoomControl: true,
-            
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-       
-        const findMe=() =>{
-               setPosition(
-                {
-                    lat: defaultLocation.lat,
-                    lng: defaultLocation.lng
-                  }
-                );
+            if (meal.host_id === userId) {
+                return hosted[selectedIndex];
+            }
+            if (meal.me > 0) {
+                return attended[selectedIndex];
+            }
+            return available[selectedIndex];
+        }
+        const MyGoogleMap = (props) => {
+            const google = window.google;
+            const [position, setPosition] = useState(defaultLocation);
+            const myOptions = {
+                zoom: 8,
+                mapTypeControlOptions: {
+                    //style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+                    //position: google.maps.ControlPosition.TOP_CENTER,
+
+                    mapTypeIds: []
+                }, // here´s the array of controls
+                disableDefaultUI: true, // a way to quickly hide all controls
+                mapTypeControl: true,
+                scaleControl: true,
+                zoomControl: true,
+
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            const findMe = () => {
+                setPosition({ ...defaultLocation });
                 console.log(position);
             }
 
-           
-        return <GoogleMap
-            defaultZoom={8}
-            defaultCenter={{ lat: defaultLocation.lat, lng: defaultLocation.lng }}
-            onClick={() => onMapClick(this)}
-            center ={{ lat: position.lat, lng: position.lng }}
-            options={myOptions}
-        >
 
-            {meals.map(meal => {
-                const isSelected = selectedMeal === meal.id;
-                const icon = getMealIcon(meal, userId, isSelected);
-                const markerSize= isSelected?40:30;
-                return <div name="marker" key={meal.id} className="marker-style" title="meal marker">
-                    <Marker
-                        position={{ lat: meal.location.y, lng: meal.location.x }}
-                        onClick={() => onMarkerClick(meal)}
-                        icon={
-                            {
-                                url: icon,
-                                scaledSize: { width: markerSize, height: markerSize, widthUnit: "px" }
-                            }}
-                    />
-                </div>
-            }
-            )}
+            return <GoogleMap
+                defaultZoom={8}
+                defaultCenter={{ lat: defaultLocation.lat, lng: defaultLocation.lng }}
+                onClick={() => onMapClick(this)}
+                center={{ lat: position.lat, lng: position.lng }}
+                options={myOptions}
+            >
 
-              <Marker
-                icon="https://www.robotwoods.com/dev/misc/bluecircle.png"
-                position={defaultLocation}/>
-            <Button onClick={()=>{findMe()}}  style={{position:"fixed", right:"33px", top:"55px"}}  >
-                Current Location
-            </Button>
+                {meals.map(meal => {
+                    const isSelected = selectedMeal === meal.id;
+                    const icon = getMealIcon(meal, userId, isSelected);
+                    const markerSize = isSelected ? 40 : 30;
+                    return <div name="marker" key={meal.id} className="marker-style" title="meal marker">
+                        <Marker
+                            position={{ lat: meal.location.y, lng: meal.location.x }}
+                            onClick={() => onMarkerClick(meal)}
+                            icon={
+                                {
+                                    url: icon,
+                                    scaledSize: { width: markerSize, height: markerSize, widthUnit: "px" }
+                                }}
+                        />
+                    </div>
+                }
+                )}
 
-        </GoogleMap>;
-    }
-    const MapWithMarker = withScriptjs(withGoogleMap(MyGoogleMap));
+                <Marker
+                    icon="https://www.robotwoods.com/dev/misc/bluecircle.png"
+                    position={defaultLocation} />
+                <img src={myLocation}
+                    onClick={() => { findMe() }} style={{ position: "fixed", right: "33px", top: "55px" }} />
 
-    return (
-        <MapWithMarker
-            yesIWantToUseGoogleMapApiInternals
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `100%`, width:`100%` }} />}
-            mapElement={<div style={{ height: `100%`, width:`100%` }} />}
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?libraries=places&key=${GOOGLE_MAPS_API_KEY}`}
-        />
-    )
-});
+            </GoogleMap>;
+        }
+        const MapWithMarker = withScriptjs(withGoogleMap(MyGoogleMap));
+
+        return (
+            <MapWithMarker
+                yesIWantToUseGoogleMapApiInternals
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `100%`, width: `100%` }} />}
+                mapElement={<div style={{ height: `100%`, width: `100%` }} />}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?libraries=places&key=${GOOGLE_MAPS_API_KEY}`}
+            />
+        )
+    });
 
 export default MealMapShow;
