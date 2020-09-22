4e5e099bd0d285f5cec66ea04c52f8fdb70cf1b2
zanooda <zanooda@ya.ru>
2020-09-22 13:34:48 +0300

    connection pool at meals.js

--- routes/api/meals.js
+++ routes/api/meals.js
@@ -56,9 +56,8 @@ router.get("/:id", async (req, response) => {
 // @desc get a list of meals created by me
 // @access Public
 router.get("/my/:id", async (req, response) => {
-  const client = new Client(currentConfig);
   console.log("get my meals by user id: " + JSON.stringify(req.params));
-  const  userId = req.params.id;
+  const userId = req.params.id;
   if (userId == "undefined") {
     console.log("error, empty id");
     response.status(400).json("Error in geting my meals: empty");
@@ -72,7 +71,7 @@ router.get("/my/:id", async (req, response) => {
       WHERE meal_id=m.id), 
     0 as attend_status, m.*, u.name  AS host_name FROM meals  AS m JOIN users AS u on m.host_id = u.id 
     WHERE m.date>now() AND host_id=$1`;
-  await client.connect();
+  const client = await pool.connect();
   client.query(SQLquery, [userId])
     .then(resp => {
       return response.json(resp.rows);
@@ -83,7 +82,7 @@ router.get("/my/:id", async (req, response) => {
     })
     .finally(()=>
     {
-        client.end();
+        client.release();
     });
 });
 
@@ -91,7 +90,6 @@ router.get("/my/:id", async (req, response) => {
 // @desc get a list of meals where the user attends
 // @access Public
 router.get("/attends/:id", async (req, response) => {
-  const client = new Client(currentConfig);
   console.log("get meals where user attends: " + JSON.stringify(req.params));
   if (req.params.id == "undefined") {
     console.log("error, empty id");
@@ -105,7 +103,7 @@ router.get("/attends/:id", async (req, response) => {
            WHERE  meal_id=m.id AND attends.user_id=$1),
         m.*, u.name AS host_name, u.id AS host_id FROM meals  AS m JOIN users AS u ON m.host_id = u.id
       ) AS sel WHERE attend_status > 0`;
-  await client.connect();
+  const client = await pool.connect();
   client.query(SQLquery, [req.params.id])
     .then(resp => {
       return response.json(resp.rows);
@@ -115,7 +113,7 @@ router.get("/attends/:id", async (req, response) => {
       return response.status(500).json(err);
     })
     .finally(() => {
-      client.end();
+      client.release();
     })
 });
 
@@ -123,7 +121,6 @@ router.get("/attends/:id", async (req, response) => {
 // @desc get a list of users attending a meal
 // @access Public
 router.get("/guests/:meal_id", async (req, response) => {
-  const client = new Client(currentConfig);
   console.log("Get users by meal_id: " + JSON.stringify(req.params));
   const meal_id = req.params.meal_id;
   if (isNaN(meal_id)) {
@@ -135,11 +132,7 @@ router.get("/guests/:meal_id", async (req, response) => {
   const SQLquery = `SELECT a.user_id, u.name FROM attends as a  
    INNER JOIN users as u ON a.user_id=u.id WHERE meal_id=$1`;
   console.log(SQLquery);
-  await client.connect()
-  .then(()=>console.log('Connected.'))
-  .catch(err => { 
-    console.error(`get guest for a meal: failed to connect, ${JSON.stringify(err)}`); 
-  });
+  const client = await pool.connect();
   client.query(SQLquery, [meal_id])
     .then(resp => {
       console.error(`Query result: ${JSON.stringify(resp.rows)}`);
@@ -151,7 +144,7 @@ router.get("/guests/:meal_id", async (req, response) => {
     }
     )
     .finally(()=>{
-      client.end();
+      client.release();
     })
 });
 
@@ -164,8 +157,7 @@ router.post("/image", async (req, response) => {
   const meal_id = req.body.meal_id;
   let image_path = req.body.image_path;
   let image_id = req.body.image_id;
-  const client = new Client(currentConfig);
-  await client.connect();
+  const client = await pool.connect();
  
   if (isNaN(image_id) || isNaN(meal_id)) {
     return response.status(500).json("Bad params: image_id");
@@ -182,7 +174,7 @@ router.post("/image", async (req, response) => {
       return response.status(500).json("failed to select random image: " + err);
     })
     .finally(() => {
-      client.end();
+      client.release();
     });
 });
 // @route POST api/meals/ - create a meal
@@ -195,10 +187,9 @@ router.post("/", async (req, response) => {
   if (!isValid) {
     return response.status(400).json(errors);
   }
-  const client = new Client(currentConfig);
 
   console.log(`add meal - start, ${JSON.stringify(req.body)}`);
-  await client.connect();
+  const client = await pool.connect();
   //todo: insert image
   const query = `INSERT INTO meals (
     name, type, location, address, guest_count, host_id, date, visibility, description)
@@ -230,7 +221,7 @@ router.post("/", async (req, response) => {
     })
     .finally(() =>
     {
-      client.end();
+      client.release();
     })
 });
 
@@ -244,10 +235,9 @@ router.delete("/:meal_id", async (req, response) => {
   if (isNaN(mealId)) {
     return response.status(400).json(`mealId: wrong meal id format: ${mealId}.`);
   }
-  const client = new Client(currentConfig);
 
   console.log(`delete ${mealId}`);
-  await client.connect();
+  const client = await pool.connect();
 
   client.query('DELETE FROM meals WHERE id=$1',
     [mealId])
@@ -261,7 +251,7 @@ router.delete("/:meal_id", async (req, response) => {
       response.status(500).json(e);
     })
     .finally(() => {
-      client.end();
+      client.release();
     });
 });
 
