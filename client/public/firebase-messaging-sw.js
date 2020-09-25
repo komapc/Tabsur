b1856120cd98e76e69b9062d60e1566036199e2b
zanooda <zanooda@ya.ru>
2020-09-26 00:38:11 +0300

    importScripts... 5.9.4 => 7.5.0

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/7.5.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.5.0/firebase-messaging.js");
 firebase.initializeApp({
   messagingSenderId: "156567484209"
 });
