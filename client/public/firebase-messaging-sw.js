1595cebcef5c2db56e2ec71c43d44b492526ce17
zanooda <zanooda@ya.ru>
2020-09-25 22:17:34 +0300

    fix

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
-firebase.initializeApp({
-  messagingSenderId: "156567484209"
-});
+// firebase.initializeApp({
+//   messagingSenderId: "156567484209"
+// });
 const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function(payload) {
   const promiseChain = clients.matchAll({
