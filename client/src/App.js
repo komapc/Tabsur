b977c7f5cf1de9e94b775bd323e5f2da7e7353b7
mark <komapc@gmail.com>
2020-09-27 23:57:42 +0300

    localStorage - try/catch

--- client/src/App.js
+++ client/src/App.js
@@ -28,24 +28,33 @@ import { messaging } from "../src/init-fcm";
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 
-// Check for token to keep user logged in
-if (localStorage.jwtToken) {
-  // Set auth token header auth
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(token);
-  // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
-  // Check for expired token
-  const currentTime = Date.now() / 1000; // to get in milliseconds
-  if (decoded.exp < currentTime) {
-    // Logout user
-    store.dispatch(logoutUser());
 
-    // Redirect to login
-    window.location.href = "./login";
+try
+{
+  // Check for token to keep user logged in
+  if (localStorage.jwtToken) {
+    // Set auth token header auth
+    const token = localStorage.jwtToken;
+    setAuthToken(token);
+    // Decode token and get user info and exp
+    const decoded = jwt_decode(token);
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+    // Check for expired token
+    const currentTime = Date.now() / 1000; // to get in milliseconds
+    if (decoded.exp < currentTime) {
+      // Logout user
+      store.dispatch(logoutUser());
+
+      // Redirect to login
+      window.location.href = "./login";
+    }
   }
+}
+catch (e)
+{
+  console.error("");
+  console.error(e);
 }
 const enableMessaging = false;
 if (enableMessaging)
