1afe4635243260a06f75b1c048a6c8055995b22c
mark <komapc@gmail.com>
2020-09-27 22:06:31 +0300

    warnings

--- client/src/components/layout/BackButton.js
+++ client/src/components/layout/BackButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { withRouter } from "react-router-dom";
 import backButton from "../../resources/back_button.svg";
 const BackButton = (props) => {
@@ -9,7 +9,6 @@ const BackButton = (props) => {
         hasHistory? props.history.goBack() : props.history.push('/');
     }
     return (
-
         <>
             <img width="20px"
                 alt="back"
