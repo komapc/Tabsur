fbeb908ee96413986024b836b8b9d7e2134c35c1
mark <komapc@gmail.com>
2020-09-28 13:43:29 +0300

    uncomment C:\Dev\MERN\client\public\firebase-messaging-sw.js

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,56 +1,56 @@
-// importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
-// importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
-// // firebase.initializeApp({
-// //   messagingSenderId: "156567484209"
-// // });
-// try
-// {
-//   const messaging = firebase.messaging();
-//   messaging.setBackgroundMessageHandler(function(payload) {
-//     const promiseChain = clients.matchAll({
-//       type: "window",
-//       includeUncontrolled: true
-//     })
-//     .then(windowClients => {
-//       for (let i = 0; i < windowClients.length; i++) {
-//         const windowClient = windowClients[i];
-//         windowClient.postMessage(payload);
-//       }
-//       return registration.showNotification(payload.data.title, payload.data);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-//     return promiseChain;
-//   });
-// }
-// catch(err)
-// {
-//   console.log(`Something failed, probably setBackgroundMessageHandler: ${err}.`)
-// }
-// self.addEventListener('notificationclick', function(event) {
-//   try
-//   {
-//     const target = event.notification.data.click_action || '/';
-//     event.notification.close();
-//     event.waitUntil(clients.matchAll({
-//       type: 'window',
-//       includeUncontrolled: true
-//     }).then(function(clientList) {
-//       for (var i = 0; i < clientList.length; i++) {
-//         var client = clientList[i];
-//         if (client.url === target && 'focus' in client) {
-//           return client.focus();
-//         }
-//       }
-//       return clients.openWindow(target);
-//     }))
-//     .catch((err) => {
-//       console.error(err);
-//     });
-//   }
-//   catch(err)
-//   {
-//     console.error(err);
-//   }
+importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
+// firebase.initializeApp({
+//   messagingSenderId: "156567484209"
 // });
+try
+{
+  const messaging = firebase.messaging();
+  messaging.setBackgroundMessageHandler(function(payload) {
+    const promiseChain = clients.matchAll({
+      type: "window",
+      includeUncontrolled: true
+    })
+    .then(windowClients => {
+      for (let i = 0; i < windowClients.length; i++) {
+        const windowClient = windowClients[i];
+        windowClient.postMessage(payload);
+      }
+      return registration.showNotification(payload.data.title, payload.data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+    return promiseChain;
+  });
+}
+catch(err)
+{
+  console.log(`Something failed, probably setBackgroundMessageHandler: ${err}.`)
+}
+self.addEventListener('notificationclick', function(event) {
+  try
+  {
+    const target = event.notification.data.click_action || '/';
+    event.notification.close();
+    event.waitUntil(clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true
+    }).then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url === target && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      return clients.openWindow(target);
+    }))
+    .catch((err) => {
+      console.error(err);
+    });
+  }
+  catch(err)
+  {
+    console.error(err);
+  }
+});
