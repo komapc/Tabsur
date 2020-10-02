707e826e5b24369222983a5cd1921ef94a342e97
zanooda <zanooda@ya.ru>
2020-10-02 22:02:24 +0300

    attach user to req at authentificateJWT.js

--- routes/authenticateJWT.js
+++ routes/authenticateJWT.js
@@ -5,14 +5,13 @@ const authenticateJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (authHeader) {
         const token = authHeader.split(' ')[1];
-        console.log(`token: ${token}`);
+        //console.log(`token: ${token}`);
         jwt.verify(token, keys.secretOrKey, (err, user) => {
             if (err) {
                 return res.sendStatus(403);
             }
-
-            //req.user = user;
-            console.log(JSON.stringify(user));
+            req.user = user;
+            //console.log(JSON.stringify(user));
             next();
         });
     } else {
