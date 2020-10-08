2fd067f09c889f595921d749f128dbf567991591
mark <komapc@gmail.com>
2020-10-08 16:33:01 +0300

    fix click on follower

--- client/src/components/users/Friend.js
+++ client/src/components/users/Friend.js
@@ -7,17 +7,17 @@ import CardHeader from '@material-ui/core/CardHeader';
 
 //one item of the list
 const Friend = (props) => {
-  return <div 
+  return <div
     style={{ width: '100%', borderBottomColor: 'lightgray', borderBottomWidth: '1px', borderBlockEndStyle: 'solid' }}>
-     <Link to={`user/${props.user_id}`} >
+    <Link to={`user/${props.user_id}`} >
       <CardHeader
         avatar={<Avatar aria-label="recipe" style={{ backgroundColor: '#13A049' }}>
           {props.name[0].toUpperCase()}
         </Avatar>}
         title={props.name}>
       </CardHeader>
-       </Link> 
-     </div>
+    </Link>
+  </div>
 }
 
 export default Friend;
