08fc74f68c40e19d71fb06a41e468f7fb5e84b54
mark <komapc@gmail.com>
2020-10-09 15:42:55 +0300

    Remove guest step

--- client/src/components/meals/CreateMeal/NameStep.js
+++ client/src/components/meals/CreateMeal/NameStep.js
@@ -9,17 +9,30 @@ const NameStep = props => {
     <div className="wizard-container row ">
 
       <Grid container spacing={4}>
-        <Box m={2} p={3}>
-          <TextField
+        <Box m={2} p={1}>
+          <TextField variant="outlined"
             className='wizard-description  justify-content-center' id="name"
             onChange={update} value={props.form.name} label="Meal Name" />
 
         </Box >
-        <Box m={2} p={3}>
-          <TextField className='wizard-description' id="description" 
+        <Box m={2} p={1}>
+          <TextField className='wizard-description' id="description"  variant="outlined" 
             onChange={update} value={props.form.description}
             placeholder="Describe the meal" label="Description" />
         </Box >
+        <Box m={2} p={1} >
+        <TextField variant="outlined"
+        width={1}
+          onChange={update}
+          value={props.form.guestCount}
+          error={props.form.guestCount === ""}
+          type="Number"
+          id="guestCount"
+          label="Guest Count"
+          placeholder="Guest Count"
+          helperText={props.form.guestCount < 0 || props.form.guestCount > 100 ? "Wrong number" : ""}
+        />
+      </Box >
       </Grid>
     </div>
 
