f9b4abde558a57de4e73fb734d55b90170c369b4
mark <komapc@gmail.com>
2020-10-14 16:19:42 +0300

    Sort chats

--- client/src/components/chat/ChatList.js
+++ client/src/components/chat/ChatList.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { getChatUsers } from "../../actions/chatActions";
@@ -6,7 +6,7 @@ import ChatListItem from "./ChatListItem";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import loadingGIF from "../../resources/animation/loading.gif";
-
+import Typography from '@material-ui/core/Typography';
 class ChatList extends Component {
 
   constructor(props) {
@@ -22,8 +22,8 @@ class ChatList extends Component {
       this.props.setFabVisibility(false);
       this.props.setSwipability(true);
     }
-    
-    this.setState({notificationsCount:this.props.notificationsCount});
+
+    this.setState({ notificationsCount: this.props.notificationsCount });
   }
 
   componentDidMount() {
@@ -38,26 +38,24 @@ class ChatList extends Component {
   };
   showList = () => {
     if (this.state.users.length === 0) {
-      return <div>No messages yet</div>
+      return <Typography>No messages yet</Typography>
     }
-    return <div className="map-meal-info" style={{ width: '100%' }}>
-      {
-        this.state.users.map(user => {
-          const sender = user.sender;
-          const receiver = user.receiver;
-          const partner = this.props.auth.user.id !== sender ? sender : receiver;
-          return <div key={user.id}>
-            <ChatListItem user={user} partner={partner} />
-          </div>
-        }
-        )}
-    </div>
+    return this.state.users.sort((a, b) => a.created_at < b.created_at ? 1 : -1)
+    .map(user => {
+      const sender = user.sender;
+      const receiver = user.receiver;
+      const partner = this.props.auth.user.id !== sender ? sender : receiver;
+      return <Typography key={user.id}>
+        <ChatListItem user={user} partner={partner} />
+      </Typography>
+    }
+    )
   }
+
   render() {
-    
+
     console.log(`notifications count: ${this.props.notificationsCount}`);
-    return (
-      <div className="main">
+    return <Fragment>
         <AppBar position="sticky">
           <Toolbar> CHAT
             {/* CHAT ({this.state.notificationsCount}) */}
@@ -67,9 +65,9 @@ class ChatList extends Component {
           this.state.loading ?
             <img src={loadingGIF} alt="loading" /> : this.showList()
         }
-      </div>
-    );
-  }}
+      </Fragment>
+  }
+}
 
 const mapStateToProps = state => ({
   auth: state.auth,
