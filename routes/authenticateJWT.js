6cd63b71829d4f8fe64f5913d59d96fc80a57589
mark <komapc@gmail.com>
2020-10-21 12:58:47 +0300

    log & return errors

--- routes/authenticateJWT.js
+++ routes/authenticateJWT.js
@@ -1,17 +1,19 @@
-const jwt = require('jsonwebtoken');
+import { verify } from 'jsonwebtoken';
 const authenticateJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (authHeader) {
         const token = authHeader.split(' ')[1];
-        jwt.verify(token, process.env.SECRET_OR_KEY, (err, user) => {
+        verify(token, process.env.SECRET_OR_KEY, (err, user) => {
             if (err) {
-                return res.sendStatus(403);
+                console.error(err);
+                return res.status(403).json(err);
             }
             req.user = user;
             next();
         });
     } else {
-        res.sendStatus(401);
+        console.error(err);
+        return res.status(401).json(err);
     }
 };
-module.exports = authenticateJWT;
+export default authenticateJWT;
