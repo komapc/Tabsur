84f878eec46264b76ab14746beda94e255de4863
mark <komapc@gmail.com>
2020-10-27 17:50:14 +0200

    simpify

--- client/src/components/layout/BackBarMui.js
+++ client/src/components/layout/BackBarMui.js
@@ -4,18 +4,15 @@ import IconButton from '@material-ui/core/IconButton';
 
 const BackBarMui = (props) => {
     return (
-        <React.Fragment>
-            <IconButton aria-label="back" size="medium" onClick={
-                (e) => {
-                    console.log("back clicked");
-                    const hasHistory = props.history.length>2;
-                    //console.log("Length: " + hasHistory);
-                    hasHistory? props.history.goBack(e) : props.history.push('/');
-                }
-            }>
-                <ArrowBackIcon fontSize="inherit" />
-            </IconButton>
-        </React.Fragment>
+        <IconButton aria-label="back" size="medium" onClick={
+            (e) => {
+                console.log("back clicked");
+                const hasHistory = props.history.length > 2;
+                hasHistory ? props.history.goBack(e) : props.history.push('/');
+            }
+        }>
+            <ArrowBackIcon fontSize="inherit" />
+        </IconButton>
     );
 }
 
