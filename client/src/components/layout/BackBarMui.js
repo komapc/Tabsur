f7542ce1acbd86fa3be65c35d6474f2569ec48bb
mark <komapc@gmail.com>
2020-10-27 19:56:46 +0200

    handleClick

--- client/src/components/layout/BackBarMui.js
+++ client/src/components/layout/BackBarMui.js
@@ -3,16 +3,15 @@ import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import IconButton from '@material-ui/core/IconButton';
 
 const BackBarMui = (props) => {
+    const handleClick = (e) => {
+        console.log("back clicked");
+        const hasHistory = props.history.length > 2;
+        hasHistory ? props.history.goBack(e) : props.history.push('/');
+    }
     return (
-        <IconButton aria-label="back" size="medium" onClick={
-            (e) => {
-                console.log("back clicked");
-                const hasHistory = props.history.length > 2;
-                hasHistory ? props.history.goBack(e) : props.history.push('/');
-            }
-        }>
-            <ArrowBackIcon fontSize="inherit" />
-        </IconButton>
+        <IconButton aria-label="back" size="medium" onClick={handleClick} className={props.className}>
+            <ArrowBackIcon />
+        </IconButton >
     );
 }
 
