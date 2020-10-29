5a442cf3257a9325fc38f685e4bcd9b9a09a86f0
zanooda <zanooda@ya.ru>
2020-10-29 10:42:58 +0200

    another one

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -19,7 +19,7 @@ export const registerUser = (userData, history) => dispatch => {
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
-        payload: err.response.data
+        payload: err
       })
     );
 };
