db1e3d4b8e6b5142a689b86c612e5f1d4236d945
zanooda <zanooda@ya.ru>
2020-11-10 13:24:51 +0200

    tryAuth

--- routes/api/attends.js
+++ routes/api/attends.js
@@ -1,5 +1,5 @@
 const pool = require("../db.js");
-const authenticateJWT = require('../authenticateJWT.js');
+const {authenticateJWT} = require('../authenticateJWT.js');
 var addNotification = require('./notificationsPush');
 var express = require('express');
 var router = express.Router();
