33f349b0689e0a3a706b08807c9a9e1522511340
zanooda <zanooda@ya.ru>
2020-12-17 19:46:31 +0200

    use react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const baseUrl = document.baseURI;
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<BrowserRouter basename={baseUrl}>
+    <Provider store={store}>
+        <App />
+    </Provider>
+</BrowserRouter>, 
+rootElement);
