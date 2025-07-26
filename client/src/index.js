ca80ae52011641b73a1c28ffd27825ea4db1d310
Mark <komapc@gmail.com>
2025-07-26 17:14:09 +0300

    import { createRoot } from 'react-dom/client';

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
 import store from "./store";
 import { BrowserRouter } from 'react-router-dom';
 
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 // const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const baseUrl = document.baseURI;
-const rootElement = document.getElementById('root');
 
-ReactDOM.render(<BrowserRouter basename={baseUrl}>
+root.render(
+  <BrowserRouter basename={baseUrl}>
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
-</BrowserRouter>, 
-rootElement);
+  </BrowserRouter>
+);
