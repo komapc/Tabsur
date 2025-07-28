09ee515b0d92961a3761a66b468100c911f18bea
Mark <komapc@gmail.com>
2025-07-28 20:22:16 +0300

    Fix db

--- db/migrate.sh
+++ db/migrate.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env sh
 
-export FLYWAY_LOCATIONS=filesystem:/tmp/migrations
-export FLYWAY_URL='jdbc:postgresql://host.docker.internal:5432/coolanu'
-export FLYWAY_USER=coolanu
-export FLYWAY_PASSWORD='coolanu'
+# Set Flyway environment variables
+export FLYWAY_LOCATIONS="filesystem:/flyway/sql"
+export FLYWAY_URL="jdbc:postgresql://coolanu-db:5432/coolanu"
+export FLYWAY_USER="coolanu"
+export FLYWAY_PASSWORD="coolanu"
 
-docker pull flyway/flyway:6.3.1
+# Attempt to pull the Flyway image (but don't fail if it doesn't work)
+docker pull flyway/flyway:6.3.1 || true
 
-docker container ls -a --filter "name=coolanu-migration" -q | xargs docker rm -f
+# Get container ID, if it exists
+CONTAINER_ID=$(docker container ls -a --filter "name=coolanu-migration" -q)
 
-docker run \
-    --name "coolanu-migration" \
-    -v $(pwd)/db/migrations:/tmp/migrations \
-    -e FLYWAY_LOCATIONS \
-    -e FLYWAY_URL \
-    -e FLYWAY_USER \
-    -e FLYWAY_PASSWORD \
-    --rm \
-    flyway/flyway:6.3.1 \
-    migrate
+# Stop and remove the container if it exists
+if [ ! -z "$CONTAINER_ID" ]; then
+  docker stop "$CONTAINER_ID"
+  docker rm "$CONTAINER_ID"
+fi
+
+# Run the Flyway migration
+docker run --rm \
+  --name "coolanu-migration" \
+  -v "$(pwd)/migrations:/flyway/sql" \
+  -e FLYWAY_LOCATIONS \
+  -e FLYWAY_URL \
+  -e FLYWAY_USER \
+  -e FLYWAY_PASSWORD \
+  flyway/flyway:6.3.1 \
+  migrate
