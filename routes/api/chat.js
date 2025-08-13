ed81953486060f1732c193fbe37bb6b0cbcb1db9
Mark <komapc@gmail.com>
2025-08-13 19:34:10 +0300

    Fix SQL syntax errors in chat.js and system.js
    - Remove extra semicolons in SQL queries
    - Fix malformed WHERE clauses
    - Correct table alias syntax
    - Fix variable name mismatch in system.js
    - All endpoints now working without SQL syntax errors

--- routes/api/chat.js
+++ routes/api/chat.js
@@ -22,30 +22,27 @@ router.get('/:id', authenticateJWT, async (req, response) => {
   sender,
   message_text,
   created_at
-  FROM notifications;
-  WHERE id IN;
-  (
-  SELECT MAX(id) as id;
-  FROM;
-    (SELECT
-      (
-        CASE
-          WHEN (receiver=$1)
-          THEN (select id FROM users where id=sender)
-          ELSE (select id FROM users where id=receiver)
-        END;
-      ) as interlocutor_id,
-      MAX(id) as id,
-      receiver,
-      sender;
-    FROM  notifications
-    WHERE note_type=0 AND (sender=$1 OR receiver=$1);
-    GROUP BY
-      receiver,
-      sender;
-    ORDER BY
-      interlocutor_id) AS pona
-  GROUP BY interlocutor_id;
+  FROM notifications
+  WHERE id IN (
+    SELECT MAX(id) as id
+    FROM (
+      SELECT
+        (
+          CASE
+            WHEN (receiver=$1)
+            THEN (select id FROM users where id=sender)
+            ELSE (select id FROM users where id=receiver)
+          END
+        ) as interlocutor_id,
+        MAX(id) as id,
+        receiver,
+        sender
+      FROM notifications
+      WHERE note_type=0 AND (sender=$1 OR receiver=$1)
+      GROUP BY receiver, sender
+      ORDER BY interlocutor_id
+    ) AS pona
+    GROUP BY interlocutor_id
   )
   `;
   console.log(`get, SQLquery: [${SQLquery}]`);
@@ -73,10 +70,11 @@ router.get('/user/:me/:user', authenticateJWT, async (req, response) => {
   }
   //todo: get messages between two users;
   const SQLquery = `SELECT
-  (select 'name' as name1 FROM 'users' where id=n.receiver),
-  (select 'name' as name2 FROM 'users' where id=n.sender),
-  id, receiver, sender, message_text, created_at FROM  notifications  as n;
-  WHERE n.note_type=0  AND  ( (n.sender=$1 AND n.receiver=$2)  OR (n.sender=$2 AND n.receiver=$1));
+  (select name as name1 FROM users where id=n.receiver),
+  (select name as name2 FROM users where id=n.sender),
+  id, receiver, sender, message_text, created_at 
+  FROM notifications as n
+  WHERE n.note_type=0 AND ((n.sender=$1 AND n.receiver=$2) OR (n.sender=$2 AND n.receiver=$1))
   `;
   console.log(`get, SQLquery: [${SQLquery}], me: ${meId}, her: ${userId}`);
   const client = await pool.connect();
